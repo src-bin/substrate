d9fe9e3680f5b3e0c033e60c6e8619594f61f936
Richard Crowley <r@rcrowley.org>
2020-04-28 21:40:42 +0000

    Sweet type safety, bro

---
+++ policies/document.go
@@ -0,0 +1,44 @@
+package policies
+
+import "fmt"
+
+type Document struct {
+	Version   version
+	Statement []Statement
+}
+
+type Statement struct {
+	Effect    Effect
+	Principal Principal
+	Action    []string
+	Resource  []string
+}
+
+type Effect string
+
+const (
+	Allow Effect = "Allow" // default, thanks to MarshalJSON
+	Deny  Effect = "Deny"
+)
+
+func (e Effect) MarshalJSON() ([]byte, error) {
+	switch e {
+	case Allow, Deny:
+	case "":
+		e = Allow
+	default:
+		return nil, fmt.Errorf("invalid Effect %#v", e)
+	}
+	return []byte(fmt.Sprintf("%#v", e)), nil
+}
+
+type Principal struct {
+	AWS     []string `json:",omitempty"`
+	Service []string `json:",omitempty"`
+}
+
+type version struct{}
+
+func (version) MarshalJSON() ([]byte, error) {
+	return []byte(`"2012-10-17"`), nil
+}
