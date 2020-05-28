e58e33d6731ae91cb65781bdc05d1c8ce4108adc
Richard Crowley <r@rcrowley.org>
2020-05-28 13:28:26 +0000

    Bottom portion of the secrets workflow

--- awssecretsmanager/secrets.go
+++ awssecretsmanager/secrets.go
@@ -3,7 +3,34 @@ package awssecretsmanager
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/src-bin/substrate/awsutil"
+	"github.com/src-bin/substrate/policies"
+	"github.com/src-bin/substrate/tags"
+	"github.com/src-bin/substrate/version"
 )
+
+const ResourceExistsException = "ResourceExistsException"
+
+func CreateSecret(svc *secretsmanager.SecretsManager, name string) (*secretsmanager.CreateSecretOutput, error) {
+	in := &secretsmanager.CreateSecretInput{
+		Name: aws.String(name),
+		Tags: []*secretsmanager.Tag{
+			&secretsmanager.Tag{
+				Key:   aws.String(tags.Manager),
+				Value: aws.String(tags.Substrate),
+			},
+			&secretsmanager.Tag{
+				Key:   aws.String(tags.SubstrateVersion),
+				Value: aws.String(version.Version),
+			},
+		},
+	}
+	out, err := svc.CreateSecret(in)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
 
 func DescribeSecret(svc *secretsmanager.SecretsManager, name string) (*secretsmanager.DescribeSecretOutput, error) {
 	in := &secretsmanager.DescribeSecretInput{
@@ -16,12 +43,50 @@ func DescribeSecret(svc *secretsmanager.SecretsManager, name string) (*secretsma
 	return out, nil
 }
 
+func EnsureSecret(
+	svc *secretsmanager.SecretsManager,
+	name string,
+	doc *policies.Document,
+	stage, value string,
+) (*secretsmanager.PutSecretValueOutput, error) {
+
+	_, err := CreateSecret(svc, name)
+	if awsutil.ErrorCodeIs(err, ResourceExistsException) {
+		err = nil
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := PutResourcePolicy(svc, name, doc); err != nil {
+		return nil, err
+	}
+
+	return PutSecretValue(svc, name, stage, value)
+}
+
 func GetSecretValue(svc *secretsmanager.SecretsManager, name, stage string) (*secretsmanager.GetSecretValueOutput, error) {
 	in := &secretsmanager.GetSecretValueInput{
 		SecretId:     aws.String(name),
 		VersionStage: aws.String(stage),
 	}
 	out, err := svc.GetSecretValue(in)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func PutResourcePolicy(svc *secretsmanager.SecretsManager, name string, doc *policies.Document) (*secretsmanager.PutResourcePolicyOutput, error) {
+	docJSON, err := doc.JSON()
+	if err != nil {
+		return nil, err
+	}
+	in := &secretsmanager.PutResourcePolicyInput{
+		ResourcePolicy: aws.String(docJSON),
+		SecretId:       aws.String(name),
+	}
+	out, err := svc.PutResourcePolicy(in)
 	if err != nil {
 		return nil, err
 	}
--- cmd/aws-secrets-manager/main.go
+++ cmd/aws-secrets-manager/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"time"
 
@@ -9,18 +10,44 @@ import (
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/src-bin/substrate/awssecretsmanager"
 	"github.com/src-bin/substrate/awssessions"
+	"github.com/src-bin/substrate/policies"
 	"github.com/src-bin/substrate/regions"
 	"github.com/src-bin/substrate/ui"
 )
 
+type StringSliceFlag []string
+
+func (ssf *StringSliceFlag) String() string {
+	return fmt.Sprint(*ssf)
+}
+
+func (ssf *StringSliceFlag) Set(s string) error {
+	*ssf = append(*ssf, s)
+	return nil
+}
+
 func main() {
 
 	name := flag.String("name", "", "name (in the UI) or ID (in the API) of the secret in AWS Secrets Manager")
+	principals := &StringSliceFlag{}
+	flag.Var(principals, "principal", "principal ARN to be allowed to GetSecretValue (if any are provided, the secret's policy will be updated to allow exactly and only those principals given)")
 	value := flag.String("value", "", "secret value to associate with -name (does not overwrite prior versions)")
 
 	flag.Parse()
 	if *name == "" {
 		log.Fatal("-name is required")
+	}
+
+	doc := &policies.Document{
+		Statement: []policies.Statement{
+			policies.Statement{
+				Action: []string{"secretsmanager:GetSecretValue"},
+				Principal: &policies.Principal{
+					AWS: *principals,
+				},
+				Resource: []string{"*"},
+			},
+		},
 	}
 
 	sess := awssessions.NewSession(awssessions.Config{})
@@ -29,12 +56,30 @@ func main() {
 
 	badRegions, goodRegions := []string{}, []string{}
 	for _, region := range regions.Selected() {
+
 		svc := secretsmanager.New(sess, &aws.Config{
 			Region: aws.String(region),
 		})
+
 		if *value != "" {
 
-			out, err := awssecretsmanager.PutSecretValue(svc, *name, stage, *value)
+			out, err := awssecretsmanager.EnsureSecret(
+				svc,
+				*name,
+				doc,
+				stage,
+				*value,
+			)
+			if err != nil {
+				badRegions = append(badRegions, region)
+				ui.Printf("%s in %s: %s", *name, region, err)
+				continue
+			}
+			ui.Printf("%+v", out)
+
+		} else if len(*principals) > 0 {
+
+			out, err := awssecretsmanager.PutResourcePolicy(svc, *name, doc)
 			if err != nil {
 				badRegions = append(badRegions, region)
 				ui.Printf("%s in %s: %s", *name, region, err)
@@ -56,14 +101,20 @@ func main() {
 		goodRegions = append(goodRegions, region)
 	}
 
-	if *value != "" {
-		ui.Printf("the %s stage of %s is ready to use in the following regions:", stage, *name)
+	if len(*principals) > 0 || *value != "" {
+		if len(*principals) > 0 {
+			ui.Printf("the resource policy for %s is updated in the following regions:", *name)
+		} else if *value != "" {
+			ui.Printf("the %s stage of %s is ready to use in the following regions:", stage, *name)
+		}
 		for _, region := range goodRegions {
 			ui.Printf("- %s", region)
 		}
-		ui.Printf("it is NOT ready to use in the following regions:")
-		for _, region := range badRegions {
-			ui.Printf("- %s", region)
+		if len(badRegions) > 0 {
+			ui.Printf("but NOT in these regions:")
+			for _, region := range badRegions {
+				ui.Printf("- %s", region)
+			}
 		}
 	}
 
