0140fc68b09c4a4f04cbac589206cc14aab36bbd
Richard Crowley <r@rcrowley.org>
2020-06-25 14:55:45 -0700

    Move the codegen modules someplace more reasonable

--- terraform/modules.go
+++ terraform/modules.go
@@ -1,21 +1,21 @@
 package terraform
 
-//go:generate go run ../tools/template/main.go -name intranetGlobalTemplate -o intranet-global.go ../intranet/global
+//go:generate go run ../tools/template/main.go -name intranetGlobalTemplate -o intranet-global.go modules/intranet/global
 func IntranetGlobalModule() *Directory {
 	return &Directory{intranetGlobalTemplate()}
 }
 
-//go:generate go run ../tools/template/main.go -name intranetRegionalTemplate -o intranet-regional.go ../intranet/regional
+//go:generate go run ../tools/template/main.go -name intranetRegionalTemplate -o intranet-regional.go modules/intranet/regional
 func IntranetRegionalModule() *Directory {
 	return &Directory{intranetRegionalTemplate()}
 }
 
-//go:generate go run ../tools/template/main.go -name lambdaFunctionGlobalTemplate -o lambda-function-global.go ../lambda-function/global
+//go:generate go run ../tools/template/main.go -name lambdaFunctionGlobalTemplate -o lambda-function-global.go modules/lambda-function/global
 func LambdaFunctionGlobalModule() *Directory {
 	return &Directory{lambdaFunctionGlobalTemplate()}
 }
 
-//go:generate go run ../tools/template/main.go -name lambdaFunctionRegionalTemplate -o lambda-function-regional.go ../lambda-function/regional
+//go:generate go run ../tools/template/main.go -name lambdaFunctionRegionalTemplate -o lambda-function-regional.go modules/lambda-function/regional
 func LambdaFunctionRegionalModule() *Directory {
 	return &Directory{lambdaFunctionRegionalTemplate()}
 }
