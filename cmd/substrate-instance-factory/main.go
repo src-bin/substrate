da4734b02419da9c9e928dc2628d83ecd152d1f1
Richard Crowley <r@rcrowley.org>
2020-08-25 20:52:01 -0700

    Reorder things in the instance factory
    This makes it possible to add the rest of what I need to this file.
    It's different than how I usually write simple CRUD apps but it works
    here since the POST portion needs substantially the same input as the
    GET portion to validate its input.

--- cmd/substrate-instance-factory/main.go
+++ cmd/substrate-instance-factory/main.go
@@ -30,34 +30,40 @@ func handle(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.
 		return nil, err
 	}
 
-	sess, err := awssessions.NewSession(awssessions.Config{})
-	if err != nil {
-		return nil, err
-	}
-
+	var instanceType string
+	region := event.QueryStringParameters["region"]
+	selectedRegions := strings.Split(event.StageVariables["SelectedRegions"], ",")
 	if event.HTTPMethod == "POST" {
 		values, err := url.ParseQuery(event.Body)
 		if err != nil {
 			return nil, err
 		}
-		instanceType := values.Get("instance_type")
-		region := values.Get("region")
+		instanceType = values.Get("instance_type")
+		region = values.Get("region")
 
+	}
+
+	// See if we've got a valid region or render the index page.
+	found := false
+	for _, r := range selectedRegions {
+		found = found || r == region
+	}
+	if !found {
 		v := struct {
-			Debug        string
-			Error        error
-			InstanceType string
-			Region       string
+			Debug   string
+			Error   error
+			Regions []string
 		}{
-			Debug:        string(b),
-			InstanceType: instanceType,
-			Region:       region,
+			Debug:   string(b),
+			Regions: selectedRegions,
 		}
-		body, err := lambdautil.RenderHTML(instanceTemplate(), v)
+		if region != "" {
+			v.Error = fmt.Errorf("%s is either not a valid region or is not in use in your organization", region)
+		}
+		body, err := lambdautil.RenderHTML(indexTemplate(), v)
 		if err != nil {
 			return nil, err
 		}
-
 		return &events.APIGatewayProxyResponse{
 			Body:       body,
 			Headers:    map[string]string{"Content-Type": "text/html"},
@@ -65,74 +71,79 @@ func handle(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.
 		}, nil
 	}
 
-	selectedRegions := strings.Split(event.StageVariables["SelectedRegions"], ",")
-
-	region := event.QueryStringParameters["region"]
-	found := false
-	for _, r := range selectedRegions {
-		found = found || r == region
+	sess, err := awssessions.NewSession(awssessions.Config{})
+	if err != nil {
+		return nil, err
 	}
-	if found {
+	svc := ec2.New(
+		sess,
+		&aws.Config{Region: aws.String(region)},
+	)
 
-		offerings, err := awsec2.DescribeInstanceTypeOfferings(ec2.New(
-			sess,
-			&aws.Config{Region: aws.String(region)},
-		))
-		if err != nil {
-			return nil, err
+	// We've got a region. Use it to enumerate all valid instance types.
+	offerings, err := awsec2.DescribeInstanceTypeOfferings(svc)
+	if err != nil {
+		return nil, err
+	}
+	instanceFamilies := make(map[string][]string)
+	for _, offering := range offerings {
+		instanceType := aws.StringValue(offering.InstanceType)
+		ss := strings.SplitN(instanceType, ".", 2)
+
+		// Don't bother offering some really old instance families just to save screen real estate.
+		if ss[0] == "c1" || ss[0] == "cc2" || ss[0] == "c3" || ss[0] == "m1" || ss[0] == "m2" || ss[0] == "m3" || ss[0] == "r3" || ss[0] == "t1" {
+			continue
 		}
 
-		instanceFamilies := make(map[string][]string)
-		for _, offering := range offerings {
-			instanceType := aws.StringValue(offering.InstanceType)
-			ss := strings.SplitN(instanceType, ".", 2)
-
-			// Don't bother offering some really old instance families just to save screen real estate.
-			if ss[0] == "c1" || ss[0] == "cc2" || ss[0] == "c3" || ss[0] == "m1" || ss[0] == "m2" || ss[0] == "m3" || ss[0] == "r3" || ss[0] == "t1" {
-				continue
+		instanceTypes, ok := instanceFamilies[ss[0]]
+		if ok {
+			instanceFamilies[ss[0]] = append(instanceTypes, instanceType)
+		} else {
+			instanceFamilies[ss[0]] = []string{instanceType}
+		}
+	}
+	for _, instanceTypes := range instanceFamilies {
+		sort.Slice(instanceTypes, func(i, j int) bool {
+			ssI, ssJ := strings.SplitN(instanceTypes[i], ".", 2), strings.SplitN(instanceTypes[j], ".", 2)
+			if ssI[0] < ssJ[0] {
+				return true
 			}
-
-			instanceTypes, ok := instanceFamilies[ss[0]]
-			if ok {
-				instanceFamilies[ss[0]] = append(instanceTypes, instanceType)
-			} else {
-				instanceFamilies[ss[0]] = []string{instanceType}
+			if ssI[0] > ssJ[0] {
+				return false
 			}
-		}
-		for _, instanceTypes := range instanceFamilies {
-			sort.Slice(instanceTypes, func(i, j int) bool {
-				ssI, ssJ := strings.SplitN(instanceTypes[i], ".", 2), strings.SplitN(instanceTypes[j], ".", 2)
-				if ssI[0] < ssJ[0] {
-					return true
-				}
-				if ssI[0] > ssJ[0] {
-					return false
-				}
-				m := map[string]int{
-					"nano":     1,
-					"micro":    2,
-					"small":    4,
-					"medium":   8,
-					"large":    16,
-					"xlarge":   32,
-					"2xlarge":  64,
-					"3xlarge":  96,
-					"4xlarge":  128,
-					"6xlarge":  192,
-					"8xlarge":  256,
-					"9xlarge":  288,
-					"10xlarge": 320,
-					"12xlarge": 384,
-					"16xlarge": 512,
-					"18xlarge": 576,
-					"24xlarge": 768,
-					"32xlarge": 1024,
-					"metal":    2048,
-				}
-				return m[ssI[1]] < m[ssJ[1]]
-			})
-		}
+			m := map[string]int{
+				"nano":     1,
+				"micro":    2,
+				"small":    4,
+				"medium":   8,
+				"large":    16,
+				"xlarge":   32,
+				"2xlarge":  64,
+				"3xlarge":  96,
+				"4xlarge":  128,
+				"6xlarge":  192,
+				"8xlarge":  256,
+				"9xlarge":  288,
+				"10xlarge": 320,
+				"12xlarge": 384,
+				"16xlarge": 512,
+				"18xlarge": 576,
+				"24xlarge": 768,
+				"32xlarge": 1024,
+				"metal":    2048,
+			}
+			return m[ssI[1]] < m[ssJ[1]]
+		})
+	}
 
+	// See if we've got a valid instance type or render the instance type page.
+	found = false
+	for _, instanceTypes := range instanceFamilies {
+		for _, i := range instanceTypes {
+			found = found || i == instanceType
+		}
+	}
+	if !found {
 		v := struct {
 			Debug            string
 			Error            error
@@ -143,11 +154,13 @@ func handle(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.
 			InstanceFamilies: instanceFamilies,
 			Region:           region,
 		}
+		if instanceType != "" {
+			v.Error = fmt.Errorf("%s is either not a valid instance type in %s", instanceType, region)
+		}
 		body, err := lambdautil.RenderHTML(instanceTypeTemplate(), v)
 		if err != nil {
 			return nil, err
 		}
-
 		return &events.APIGatewayProxyResponse{
 			Body:       body,
 			Headers:    map[string]string{"Content-Type": "text/html"},
@@ -155,27 +168,38 @@ func handle(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.
 		}, nil
 	}
 
+	// Let's do this!
 	v := struct {
-		Debug   string
-		Error   error
-		Regions []string
+		Debug        string
+		Error        error
+		InstanceType string
+		Region       string
 	}{
-		Debug:   string(b),
-		Regions: selectedRegions,
+		Debug:        string(b),
+		InstanceType: instanceType,
+		Region:       region,
 	}
-	if region != "" {
-		v.Error = fmt.Errorf("%s is either not a valid region or is not in use in your organization", region)
+	imageId, err := awsec2.LatestAmazonLinux2AMI(svc)
+	if err != nil {
+		v.Error = err
 	}
-	body, err := lambdautil.RenderHTML(indexTemplate(), v)
+	v.Error = fmt.Errorf("%+v", out)
+	/*
+		_, err := awsec2.RunInstances(svc)
+		if err != nil {
+			v.Error = err
+		}
+	*/
+	body, err := lambdautil.RenderHTML(instanceTemplate(), v)
 	if err != nil {
 		return nil, err
 	}
-
 	return &events.APIGatewayProxyResponse{
 		Body:       body,
 		Headers:    map[string]string{"Content-Type": "text/html"},
 		StatusCode: http.StatusOK,
 	}, nil
+
 }
 
 func main() {
