f7a65c1d0972b4e32019e39b3837c9577c3d5062
Richard Crowley <r@rcrowley.org>
2020-10-27 10:11:23 -0700

    substrate-accounts command
    It does the same thing as `cat substrate-accounts.txt` but provides a
    springboard for future fine-grained invocations.

---
+++ cmd/substrate-accounts/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go/service/organizations"
+	"github.com/src-bin/substrate/accounts"
+	"github.com/src-bin/substrate/awssessions"
+	"github.com/src-bin/substrate/roles"
+	"github.com/src-bin/substrate/version"
+)
+
+func main() {
+	flag.Parse()
+	version.Flag()
+
+	sess, err := awssessions.InMasterAccount(roles.OrganizationReader, awssessions.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	svc := organizations.New(sess)
+
+	if err := accounts.CheatSheet(svc); err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Open(accounts.CheatSheetFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	io.Copy(os.Stdout, f)
+
+}
