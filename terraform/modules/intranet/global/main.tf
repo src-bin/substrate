688e941394c3c6c6a00f6deff78878e873abfe1c
Richard Crowley <r@rcrowley.org>
2021-01-29 23:01:43 -0800

    Update ACM validation to provider 3.x.x data structure
    The validation options are now a set which Terraform distinguishes by
    disallowing enumeration. Per
    <https://github.com/hashicorp/terraform/issues/26043>, we use tolist
    here because we know definitively that there is a single SAN in the
    certificate in question and thus only ever a single name to validate.

--- terraform/modules/intranet/global/main.tf
+++ terraform/modules/intranet/global/main.tf
@@ -183,9 +183,9 @@ resource "aws_iam_user_policy_attachment" "credential-factory" {
 }
 
 resource "aws_route53_record" "validation" {
-  name    = aws_acm_certificate.intranet.domain_validation_options.0.resource_record_name
-  records = [aws_acm_certificate.intranet.domain_validation_options.0.resource_record_value]
+  name    = tolist(aws_acm_certificate.intranet.domain_validation_options)[0].resource_record_name
+  records = [tolist(aws_acm_certificate.intranet.domain_validation_options)[0].resource_record_value]
   ttl     = 60
-  type    = aws_acm_certificate.intranet.domain_validation_options.0.resource_record_type
+  type    = tolist(aws_acm_certificate.intranet.domain_validation_options)[0].resource_record_type
   zone_id = data.aws_route53_zone.intranet.zone_id
 }
