de4836d2c2dff607fbea36fead89299708196512
Richard Crowley <r@rcrowley.org>
2021-02-01 11:28:29 -0800

    Also pin archive and external provider versions for completeness

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,9 +2,17 @@
 
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "= 2.0.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "= 3.26.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "= 2.0.0"
     }
   }
   required_version = "= 0.13.6"
--- terraform/versions.tf.go
+++ terraform/versions.tf.go
@@ -7,9 +7,17 @@ func versionsTemplate() string {
 
 terraform {
   required_providers {
+    archive = {
+      source  = "hashicorp/archive"
+      version = "= 2.0.0"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = "= 3.26.0"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "= 2.0.0"
     }
   }
   required_version = "= 0.13.6"
