4a6d1e4a1ac3b25e47bf4804cc93c1bcdfead1d0
Richard Crowley <r@rcrowley.org>
2021-03-07 00:07:30 -0800

    Update to Go 1.16 and Terraform 0.14.7

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -15,5 +15,5 @@ terraform {
       version = "= 2.0.0"
     }
   }
-  required_version = "= 0.13.6"
+  required_version = "= 0.14.7"
 }
--- terraform/versions.tf.go
+++ terraform/versions.tf.go
@@ -20,7 +20,7 @@ terraform {
       version = "= 2.0.0"
     }
   }
-  required_version = "= 0.13.6"
+  required_version = "= 0.14.7"
 }
 `
 }
