5d1101c4e8e3484d3a056858dc0c3b0438df232d
Richard Crowley <r@rcrowley.org>
2021-06-07 23:56:46 -0700

    Pin Terraform version 0.15.5

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -15,5 +15,5 @@ terraform {
       version = "= 2.0.0"
     }
   }
-  required_version = "= 0.14.7"
+  required_version = "= 0.15.5"
 }
--- terraform/versions.tf.go
+++ terraform/versions.tf.go
@@ -20,7 +20,7 @@ terraform {
       version = "= 2.0.0"
     }
   }
-  required_version = "= 0.14.7"
+  required_version = "= 0.15.5"
 }
 `
 }
