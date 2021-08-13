56b460a04f7a3f89fd80c126977ed49b227a1d64
Richard Crowley <r@rcrowley.org>
2021-08-12 20:55:37 -0700

    Return the old working directory from Chdir and MustChdir

--- cmdutil/root.go
+++ cmdutil/root.go
@@ -8,15 +8,25 @@ import (
 
 const substrateRoot = "SUBSTRATE_ROOT"
 
-func Chdir() error {
-	if dirname := os.Getenv(substrateRoot); dirname != "" {
-		return os.Chdir(dirname)
+// Chdir changes the working directory to the value of the SUBSTRATE_ROOT
+// environment variable, if set and non-empty. It returns the previous working
+// directory and the error returned by os.Chdir.
+func Chdir() (oldpwd string, err error) {
+	oldpwd, err = os.Getwd()
+	if err != nil {
+		return
 	}
-	return nil
+	if dirname := os.Getenv(substrateRoot); dirname != "" {
+		err = os.Chdir(dirname)
+	}
+	return
 }
 
-func MustChdir() {
-	if err := Chdir(); err != nil {
+// MustChdir calls Chdir and terminates the process if it returns an error.
+func MustChdir() string {
+	oldpwd, err := Chdir()
+	if err != nil {
 		ui.Fatal(err)
 	}
+	return oldpwd
 }
