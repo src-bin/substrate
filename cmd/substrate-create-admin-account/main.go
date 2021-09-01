9dec2a99ff9801971c9b10c504f7d8affd4bbc92
Richard Crowley <r@rcrowley.org>
2021-08-31 23:16:28 -0700

    Push interactive IdP stuff to the end

--- cmd/substrate-create-admin-account/main.go
+++ cmd/substrate-create-admin-account/main.go
@@ -10,11 +10,14 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/aws/aws-sdk-go/service/organizations"
 	"github.com/aws/aws-sdk-go/service/route53"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
 	"github.com/src-bin/substrate/accounts"
 	"github.com/src-bin/substrate/admin"
+	"github.com/src-bin/substrate/awsiam"
 	"github.com/src-bin/substrate/awsorgs"
 	"github.com/src-bin/substrate/awsroute53"
 	"github.com/src-bin/substrate/awssecretsmanager"
@@ -29,17 +32,23 @@ import (
 	"github.com/src-bin/substrate/roles"
 	"github.com/src-bin/substrate/terraform"
 	"github.com/src-bin/substrate/ui"
+	"github.com/src-bin/substrate/users"
 	"github.com/src-bin/substrate/veqp"
 	"github.com/src-bin/substrate/version"
 )
 
 const (
-	Domain                                 = "admin"
-	Environment                            = "admin"
+	Domain      = "admin"
+	Environment = "admin"
+
+	Google = "Google"
+	Okta   = "Okta"
+
 	OAuthOIDCClientIdFilename              = "substrate.oauth-oidc-client-id"
 	OAuthOIDCClientSecretTimestampFilename = "substrate.oauth-oidc-client-secret-timestamp"
 	OktaHostnameFilename                   = "substrate.okta-hostname"
-	SAMLMetadataFilename                   = "substrate.saml-metadata.xml"
+
+	SAMLMetadataFilename = "substrate.saml-metadata.xml"
 )
 
 func main() {
@@ -79,19 +88,81 @@ func main() {
 
 	// Ensure the account exists.
 	ui.Spin("finding or creating the admin account")
-	svc := organizations.New(sess)
-	account, err := awsorgs.EnsureAccount(svc, accounts.Admin, accounts.Admin, *quality)
+	account, err := awsorgs.EnsureAccount(organizations.New(sess), accounts.Admin, accounts.Admin, *quality)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := accounts.CheatSheet(svc); err != nil {
+	if err := accounts.CheatSheet(organizations.New(sess)); err != nil {
 		log.Fatal(err)
 	}
 	ui.Stopf("account %s", account.Id)
 	//log.Printf("%+v", account)
 
-	idpName := idp(sess, account, metadata)
+	//idpName := idp(sess, account, metadata)
+	svc := iam.New(sess, &aws.Config{
+		Credentials: stscreds.NewCredentials(sess, roles.Arn(
+			aws.StringValue(account.Id),
+			roles.OrganizationAccountAccessRole,
+		)),
+	})
 
+	idpName := "IdP"
+	if strings.Contains(metadata, "google.com") {
+		idpName = Google
+	} else if strings.Contains(metadata, "okta.com") {
+		idpName = Okta
+	}
+
+	ui.Spinf("configuring %s as your organization's identity provider", idpName)
+	saml, err := awsiam.EnsureSAMLProvider(svc, idpName, metadata)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ui.Stopf("provider %s", saml.Arn)
+	//log.Printf("%+v", saml)
+
+	// Pre-create this user so that it may be referenced in policies attached to
+	// the Administrator user.  Terraform will attach policies to it later.
+	ui.Spin("finding or creating an IAM user for your Credential Factory, so it can get 12-hour credentials")
+	user, err := awsiam.EnsureUser(svc, users.CredentialFactory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(5e9) // TODO wait only just long enough for IAM to become consistent, and probably do it in EnsureUser
+	ui.Stopf("user %s", user.UserName)
+
+	// Give Okta some entrypoints in the Admin account.
+	ui.Spinf("finding or creating roles for %s to use in this admin account", idpName)
+	_, adminRolePrincipals, err := admin.AdminPrincipals(organizations.New(sess))
+	if err != nil {
+		log.Fatal(err)
+	}
+	assumeRolePolicyDocument := &policies.Document{
+		Statement: []policies.Statement{
+			policies.AssumeRolePolicyDocument(adminRolePrincipals).Statement[0],
+			policies.AssumeRolePolicyDocument(&policies.Principal{
+				AWS: []string{users.Arn(
+					aws.StringValue(account.Id),
+					users.CredentialFactory,
+				)},
+				Service: []string{"ec2.amazonaws.com"},
+			}).Statement[0],
+			policies.AssumeRolePolicyDocument(&policies.Principal{
+				Federated: []string{saml.Arn},
+			}).Statement[0],
+		},
+	}
+	//log.Printf("%+v", assumeRolePolicyDocument)
+	if _, err := admin.EnsureAdministratorRole(svc, assumeRolePolicyDocument); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := admin.EnsureAuditorRole(svc, assumeRolePolicyDocument); err != nil {
+		log.Fatal(err)
+	}
+	ui.Stop("ok")
+
+	// This must come before the Terraform run because it references the IAM
+	// roles created here.
 	admin.EnsureAdminRolesAndPolicies(sess)
 
 	// Make arrangements for a hosted zone to appear in this account so that
@@ -372,11 +443,67 @@ func main() {
 		ui.Printf("wrote %s, which you should commit to version control", OAuthOIDCClientSecretTimestampFilename)
 	}
 
+	// Google asks GSuite admins to set custom attributes user by user.  Help
+	// these poor souls out by at least telling them exactly what value to set.
+	if idpName == Google {
+		ui.Printf(
+			`set the AWS/Role custom attribute in GSuite for every authorized AWS Console user to "%s,%s"`,
+			roles.Arn(aws.StringValue(account.Id), roles.Administrator),
+			saml.Arn,
+		)
+		if _, err := ui.Prompt("press <enter> after you've configured at least one GSuite user (so you can test this)"); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Give Okta a user that can enumerate the roles it can assume.  Only Okta
+	// needs this.  Google puts more of the burden on GSuite admins.
+	if idpName == Okta {
+		ui.Spin("finding or creating a user for Okta to use to enumerate roles")
+		user, err := awsiam.EnsureUserWithPolicy(
+			svc,
+			Okta,
+			&policies.Document{
+				Statement: []policies.Statement{{
+					Action:   []string{"iam:ListAccountAliases", "iam:ListRoles"},
+					Resource: []string{"*"},
+				}},
+			},
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ui.Stopf("user %s", user.UserName)
+		//log.Printf("%+v", user)
+		if ok, err := ui.Confirm( // TODO need to find a way to remove this as it is remarkably confusing and tedious if answered incorrectly; maybe only do it if IdP metadata was changed?
+			`answering "yes" will break any existing integration - do you need to configure Okta's AWS integration? (yes/no)`,
+		); err != nil {
+			log.Fatal(err)
+		} else if ok {
+			ui.Spin("deleting existing access keys and creating a new one")
+			if err := awsiam.DeleteAllAccessKeys(svc, Okta); err != nil {
+				log.Fatal(err)
+			}
+			accessKey, err := awsiam.CreateAccessKey(svc, aws.StringValue(user.UserName))
+			if err != nil {
+				log.Fatal(err)
+			}
+			ui.Stop("ok")
+			//log.Printf("%+v", accessKey)
+			ui.Printf("Okta needs this SAML provider ARN: %s", saml.Arn)
+			ui.Printf(".. and this access key ID: %s", accessKey.AccessKeyId)
+			ui.Printf("...and this secret access key: %s", accessKey.SecretAccessKey) // TODO can't put this into substrate.onboarding.txt; shit
+			if _, err := ui.Prompt("press <enter> after you've updated your Okta configuration"); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
 	ui.Printf(
 		"next, commit modules/intranet/, modules/lambda-function/, modules/substrate/, and root-modules/%s/%s/ to version control, then run substrate-create-account as many times as you need",
 		Domain,
 		*quality,
 	)
-	ui.Printf("you should also start using <https://%s/credential-factory> or substrate-credentials to mint short-lived AWS access keys", dnsDomainName)
+	ui.Printf("you should also start using substrate-credentials or <https://%s/credential-factory> to mint short-lived AWS access keys", dnsDomainName)
 
 }
