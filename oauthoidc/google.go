3c6a237da3b350d3ae58fad89b34c729cebc6c55
Richard Crowley <r@rcrowley.org>
2021-11-13 19:15:27 -0800

    Crude pass at getting custom schema data from Google

--- oauthoidc/google.go
+++ oauthoidc/google.go
@@ -1,6 +1,25 @@
 package oauthoidc
 
-import "net/url"
+import (
+	"log"
+	"net/url"
+	"path"
+)
+
+func GoogleAdminDirectoryUser(c *Client, userKey string) ( /* TODO */ map[string]interface{}, error) {
+	body := map[string]interface{}{} // TODO better type
+	resp, err := c.GetURL(&url.URL{
+		Scheme: "https",
+		Host:   "admin.googleapis.com",
+		Path:   path.Join("/admin/directory/v1/users", userKey),
+	}, url.Values{"projection": {"full"}, "viewType": {"domain_public"}}, &body)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("resp: %+v", resp)
+	log.Printf("body: %+v", body)
+	return body, nil
+}
 
 func GooglePathQualifier() PathQualifier {
 	// TODO dynamically construct this function from <https://accounts.google.com/.well-known/openid-configuration>
