dfd9d4c0658354faec0dfb34ba96427b04b29a7b
Richard Crowley <r@rcrowley.org>
2021-11-15 22:07:42 -0800

    WIP proxy fixes for 2021.11

--- cmd/substrate-intranet/proxy.go
+++ cmd/substrate-intranet/proxy.go
@@ -31,9 +31,21 @@ func proxy(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.A
 	}
 	// TODO figure out why trailing slashes are stripped and whether it matters. It matters. It breaks Jenkins with path_part="jenkins" and --prefix="/jenkins" at /jenkins/ and /jenkins/view/all/ (so it's not just the root).
 
-	req, err := http.NewRequest(event.HTTPMethod, u.String(), strings.NewReader(event.Body))
+	var body *bytes.Buffer
+	if event.IsBase64Encoded {
+		b, err := base64.StdEncoding.DecodeString(event.Body)
+		if err != nil {
+			return lambdautil.ErrorResponse(err)
+		}
+		body = bytes.NewBuffer(b)
+	} else {
+		body = bytes.NewBufferString(event.Body)
+	}
+	log.Printf("event.Body: %+v", body.String())
+	req, err := http.NewRequest(event.HTTPMethod, u.String(), body)
 	req.Header = event.MultiValueHeaders
-	// TODO possibly remove the TE header just in case that HTTP/2-ism is destroying everything
+	req.Header.Add("X-Forwarded-Host", event.Headers["Host"])
+	//req.Header.Add("X-Forwarded-Proto", "https") // already added by API Gateway
 	req.Header.Add("X-Substrate-Intranet-Proxy-Principal", event.RequestContext.Authorizer["principalId"].(string))
 	log.Printf("req: %+v", req)
 	resp, err := http.DefaultClient.Do(req)
@@ -43,8 +55,8 @@ func proxy(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.A
 	log.Printf("resp: %+v", resp)
 	resp.Header.Del("Content-Length") // because we base64-encode
 	defer resp.Body.Close()
-	body := &bytes.Buffer{}
-	if _, err := io.Copy(base64.NewEncoder(base64.RawStdEncoding, body), resp.Body); err != nil {
+	body.Reset()
+	if _, err := io.Copy(base64.NewEncoder(base64.StdEncoding, body), resp.Body); err != nil {
 		return lambdautil.ErrorResponse(err)
 	}
 	log.Printf("body: %+v", body.String())
