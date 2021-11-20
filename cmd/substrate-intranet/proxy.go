52577418048aefe11fd4b52ce14026f065874fbe
Richard Crowley <r@rcrowley.org>
2021-11-19 21:35:41 -0800

    Remove all the sketchy shit

--- cmd/substrate-intranet/proxy.go
+++ cmd/substrate-intranet/proxy.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"encoding/base64"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -18,47 +17,10 @@ import (
 	"github.com/src-bin/substrate/lambdautil"
 )
 
-// checkRedirect uses net/http's standard hook for managing HTTP redirects to
-// propagate cookies even when differences between the domain of the original
-// request's URL and the Location header suggest it should not be. This is safe
-// in the context of this HTTP client because the destination is always
-// declared up-front.
+// checkRedirect that doesn't redirect and instead lets clients do all the
+// redirecting on their own, leaving this a request-at-a-time proxy.
 func checkRedirect(req *http.Request, via []*http.Request) error {
-
-	// Copied from net/http's defaultCheckRedirect so we don't lose infinite
-	// redirect suppression while adding cookie propagation.
-	if len(via) >= 10 {
-		return errors.New("stopped after 10 redirects")
-	}
-
-	// If the redirect is anything other than appending a '/' to the URI path,
-	// let the user-agent handle it. We only even handle these redirects
-	// because AWS API Gateway insists on stripping trailing slashes.
-	//
-	// TODO might need to reverse STRIP_PATH_PREFIX to be totally correct but
-	// I don't see how to do it in Go's HTTP client.
-	if req.URL.Path != via[0].URL.Path+"/" {
-		return http.ErrUseLastResponse
-	}
-	log.Printf("redirecting internally to %s", req.URL)
-
-	req.Method = via[0].Method
-	req.Header = via[0].Header
-
-	scheme := req.Header.Get("X-Forwarded-Proto")
-	host := req.Header.Get("X-Forwarded-Host")
-	port := req.Header.Get("X-Forwarded-Port")
-	if port != "" && (scheme == "http" && port != "80" || scheme == "https" && port != "443") {
-		host += ":" + port
-	}
-	if req.URL.Scheme == scheme && req.URL.Host == host {
-		s := req.URL.String()
-		req.URL.Scheme = via[0].URL.Scheme
-		req.URL.Host = via[0].URL.Host
-		log.Printf("rewriting internal redirect from %s to %s", s, req.URL)
-	}
-
-	return nil
+	return http.ErrUseLastResponse
 }
 
 func init() {
@@ -78,7 +40,7 @@ func proxy(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.A
 		u.Path = path.Join(u.Path, event.Path)
 	}
 	u.RawQuery = url.Values(event.MultiValueQueryStringParameters).Encode()
-	log.Printf("u: %+v, u.String(): %s", u, u)
+	//log.Printf("u: %s", u)
 
 	body, err := lambdautil.EventBodyBuffer(event)
 	if err != nil {
@@ -100,6 +62,7 @@ func proxy(ctx context.Context, event *events.APIGatewayProxyRequest) (*events.A
 	log.Printf("resp: %+v", resp)
 
 	resp.Header.Del("Content-Length") // because we base64-encode
+	// TODO reverse the Location header if there is one.
 	defer resp.Body.Close()
 	b, err := io.ReadAll(resp.Body) // TODO get rid of this superfluous allocation by using body.Reset(), io.Copy, and base64.NewEncoder
 	//log.Printf("len(b): %d, b: %#v", len(b), string(b))
