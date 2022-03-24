2bcfbec268101ad3269b145a21d3e33438d16045
Richard Crowley <r@rcrowley.org>
2022-03-24 00:44:13 -0700

    Support AWS SDK v2 in the error classifier

--- awsutil/errors.go
+++ awsutil/errors.go
@@ -1,13 +1,27 @@
 package awsutil
 
-import "github.com/aws/aws-sdk-go/aws/awserr"
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/smithy-go"
+)
 
 const RequestError = "RequestError"
 
 func ErrorCode(err error) string {
+
+	// AWS SDK for Go v1
 	if e, ok := err.(awserr.Error); ok {
 		return e.Code()
 	}
+
+	// AWS SDK for Go v2
+	var ae smithy.APIError
+	if errors.As(err, &ae) {
+		return ae.ErrorCode()
+	}
+
 	return ""
 }
 
