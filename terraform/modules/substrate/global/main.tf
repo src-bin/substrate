05033157e549b5cbc7608e839cdb6cfaab287348
Richard Crowley <r@rcrowley.org>
2022-03-27 07:20:50 -0700

    Explicitly non-interactive substrate-assume-role within Terraform runs

--- terraform/modules/substrate/global/main.tf
+++ terraform/modules/substrate/global/main.tf
@@ -2,7 +2,7 @@ data "aws_caller_identity" "current" {}
 
 data "external" "tags" {
   program = [
-    "substrate-assume-role", "-management", "-quiet", "-role=OrganizationReader",
+    "substrate-assume-role", "-management", "-non-interactive", "-quiet", "-role=OrganizationReader",
     "aws", "organizations", "list-tags-for-resource",
     "--resource-id", data.aws_caller_identity.current.account_id,
     "--query", "{Domain:Tags[?Key==`Domain`].Value|[0],Environment:Tags[?Key==`Environment`].Value|[0],Quality:Tags[?Key==`Quality`].Value|[0]}",
