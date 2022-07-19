05f0005a9cd834f8280df2e406ac88489456d68c
Richard Crowley <r@rcrowley.org>
2022-07-18 22:13:24 -0700

    Don't try xdg-open if we know it's not there
    This was cosmetically breaking `substrate credentials` in headless
    environments.

--- ui/open.go
+++ ui/open.go
@@ -8,26 +8,28 @@ import (
 // scheme and local configuration) and returns true or falls back to printing
 // the URL with instructions to open it and returns false.
 func OpenURL(s string) bool {
-
-	// This part could be safely memoized if it ever became the slow part.
-	var progname string
-	for _, progname = range []string{
-		"open",     // MacOS
-		"xdg-open", // Linux
-	} {
-		if _, err := exec.LookPath(progname); err == nil {
-			break
-		}
-	}
-
-	if progname != "" {
+	if progname := lookPath(); progname != "" {
 		Printf("opening <%s> in your web browser", s)
 		if err := exec.Command(progname, s).Start(); err != nil {
 			Fatal(err)
 		}
 		return true
-	} else {
-		Printf("please open <%s> in your web browser", s)
-		return false
 	}
+	Printf("please open <%s> in your web browser", s)
+	return false
+}
+
+// lookPath returns the program that can be used to generically open a URL in
+// the default web browser or the empty string if there isn't one. It could be
+// safely memoized if it ever became the slow part.
+func lookPath() string {
+	for _, progname := range []string{
+		"open",     // MacOS
+		"xdg-open", // Linux
+	} {
+		if _, err := exec.LookPath(progname); err == nil {
+			return progname
+		}
+	}
+	return ""
 }
