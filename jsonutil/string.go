c79f9ceac5ca6fa53926226445d6568fe5bd0ac9
Richard Crowley <r@rcrowley.org>
2022-08-03 10:38:42 -0700

    Add one-liner versions and use ui.Fatal instead

--- jsonutil/string.go
+++ jsonutil/string.go
@@ -2,15 +2,29 @@ package jsonutil
 
 import (
 	"encoding/json"
-	"log"
+
+	"github.com/src-bin/substrate/ui"
 )
+
+func MustOneLineString(document interface{}) string {
+	s, err := OneLineString(document)
+	if err != nil {
+		ui.Fatal(err)
+	}
+	return s
+}
 
 func MustString(document interface{}) string {
 	s, err := String(document)
 	if err != nil {
-		log.Fatal(err)
+		ui.Fatal(err)
 	}
 	return s
+}
+
+func OneLineString(document interface{}) (string, error) {
+	b, err := json.Marshal(document)
+	return string(b), err
 }
 
 func String(document interface{}) (string, error) {
