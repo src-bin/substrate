886d6c3d7e4c77956c0eea2870da2fc664117b72
Richard Crowley <r@rcrowley.org>
2022-10-12 22:40:01 -0700

    Be compatible with the AWS SDK's credential_process

--- awsutil/credentials.go
+++ awsutil/credentials.go
@@ -2,6 +2,7 @@ package awsutil
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/src-bin/substrate/cmdutil"
@@ -9,55 +10,64 @@ import (
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-func PrintCredentials(format *cmdutil.SerializationFormat, credentials aws.Credentials) {
+func PrintCredentials(format *cmdutil.SerializationFormat, creds aws.Credentials) {
 	switch format.String() {
 	case cmdutil.SerializationFormatEnv:
-		PrintCredentialsEnv(credentials)
+		PrintCredentialsEnv(creds)
 	case cmdutil.SerializationFormatExport:
-		PrintCredentialsExport(credentials)
+		PrintCredentialsExport(creds)
 	case cmdutil.SerializationFormatExportWithHistory:
-		PrintCredentialsExportWithHistory(credentials)
+		PrintCredentialsExportWithHistory(creds)
 	case cmdutil.SerializationFormatJSON:
-		PrintCredentialsJSON(credentials)
+		PrintCredentialsJSON(creds)
 	default:
 		ui.Fatalf("-format %q not supported", format)
 	}
 }
 
-func PrintCredentialsEnv(credentials aws.Credentials) {
+func PrintCredentialsEnv(creds aws.Credentials) {
 	fmt.Printf(
 		"AWS_ACCESS_KEY_ID=%q\nAWS_SECRET_ACCESS_KEY=%q\nAWS_SESSION_TOKEN=%q\n",
-		credentials.AccessKeyID,
-		credentials.SecretAccessKey,
-		credentials.SessionToken,
+		creds.AccessKeyID,
+		creds.SecretAccessKey,
+		creds.SessionToken,
 	)
 }
 
-func PrintCredentialsExport(credentials aws.Credentials) {
+func PrintCredentialsExport(creds aws.Credentials) {
 	fmt.Printf(
 		" export AWS_ACCESS_KEY_ID=%q AWS_SECRET_ACCESS_KEY=%q AWS_SESSION_TOKEN=%q\n",
-		credentials.AccessKeyID,
-		credentials.SecretAccessKey,
-		credentials.SessionToken,
+		creds.AccessKeyID,
+		creds.SecretAccessKey,
+		creds.SessionToken,
 	)
 }
 
-func PrintCredentialsExportWithHistory(credentials aws.Credentials) {
+func PrintCredentialsExportWithHistory(creds aws.Credentials) {
 	if terminal.IsTerminal(1) {
 		ui.Print("paste this into a shell to set environment variables (taking care to preserve the leading space):")
 	}
 	fmt.Printf(
 		` export OLD_AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" AWS_ACCESS_KEY_ID=%q OLD_AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" AWS_SECRET_ACCESS_KEY=%q OLD_AWS_SESSION_TOKEN="$AWS_SESSION_TOKEN" AWS_SESSION_TOKEN=%q; alias unassume-role='AWS_ACCESS_KEY_ID="$OLD_AWS_ACCESS_KEY_ID" AWS_SECRET_ACCESS_KEY="$OLD_AWS_SECRET_ACCESS_KEY" AWS_SESSION_TOKEN="$OLD_AWS_SESSION_TOKEN"; unset OLD_AWS_ACCESS_KEY_ID OLD_AWS_SECRET_ACCESS_KEY OLD_AWS_SESSION_TOKEN'
 `,
-		credentials.AccessKeyID,
-		credentials.SecretAccessKey,
-		credentials.SessionToken,
+		creds.AccessKeyID,
+		creds.SecretAccessKey,
+		creds.SessionToken,
 	)
 }
 
-func PrintCredentialsJSON(credentials aws.Credentials) {
+func PrintCredentialsJSON(creds aws.Credentials) {
 	ui.PrettyPrintJSON(struct {
-		aws.Credentials
-		Version int
-	}{credentials, 1})
+		AccessKeyId     string // must be "Id" not "ID" for AWS SDK credential_process
+		SecretAccessKey string
+		SessionToken    string
+		Expiration      string // must be "Expiration" not "Expires" for AWS SDK credential_process
+		Version         int
+	}{
+		creds.AccessKeyID,
+		creds.SecretAccessKey,
+		creds.SessionToken,
+		creds.Expires.Format(time.RFC3339),
+		1,
+	})
 }
