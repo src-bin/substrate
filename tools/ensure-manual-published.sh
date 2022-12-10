c68bfe9306e01473879f8e5c6a24728b800be7e9
Richard Crowley <r@rcrowley.org>
2022-12-09 22:22:10 -0800

    You muppet

--- tools/ensure-manual-published.sh
+++ tools/ensure-manual-published.sh
@@ -5,7 +5,7 @@ trap "rm -f \"$TMP\"" EXIT INT QUIT TERM
 
 # Find out if this is a tagged release. If so, note the version string. If not,
 # exit zero because we have no more work to do.
-git describe --exact-match --tags "HEAD^" >"$TMP" || exit 0
+git describe --exact-match --tags "HEAD" >"$TMP" || exit 0
 
 # Ensure we've at least pushed release notes covering this tagged release.
 curl -s "https://src-bin.com/substrate/manual/releases/" |
