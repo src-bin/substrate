6f783b49e1b83125517656746cc7d308dd665d5b
Richard Crowley <r@rcrowley.org>
2022-12-13 22:38:53 -0800

    Shippable install.sh and test-install.sh

--- install.sh
+++ install.sh
@@ -2,49 +2,97 @@
 
 set -e
 
+TMP="$(mktemp -d)"
+trap "rm -f -r \"$TMP\"" EXIT INT QUIT TERM
+
 usage() {
-    echo "Usage: $0 <prefix> <known-version>" >&2
-    echo "  <prefix>        contents of substrate.prefix for a once-paying Substrate customer" >&2
-    echo "  <known-version> version string from any past release of Substrate, even a distant past release, in full YYYY.MM-01234abc form" >&2
+    echo "Usage: $0 [-d <dirname>] -p <prefix> -v <known-version>" >&2
+    echo "  -d <dirname>       directory where the latest version of Substrate should be installed (defaults to the first writable directory on your PATH)" >&2
+    echo "  -p <prefix>        contents of substrate.prefix for a Substrate customer" >&2
+    echo "  -v <known-version> version string from any past release of Substrate, even a distant past release, in full YYYY.MM-01234abc form" >&2
     exit "$1"
 }
 
+DIRNAME="" PREFIX="" VERSION=""
 while [ "$#" -gt 0 ]
 do
     case "$1" in
+        "-d"|"--dirname") DIRNAME="$2" shift 2;;
+        "-d"*) DIRNAME="$(echo "$1" | cut -c"3-")" shift;;
+        "--dirname="*) DIRNAME="$(echo "$1" | cut -d"=" -f"2-")" shift;;
+        "-p"|"--prefix") PREFIX="$2" shift 2;;
+        "-p"*) PREFIX="$(echo "$1" | cut -c"3-")" shift;;
+        "--prefix="*) PREFIX="$(echo "$1" | cut -d"=" -f"2-")" shift;;
+        "-v"|"--version") VERSION="$2" shift 2;;
+        "-v"*) VERSION="$(echo "$1" | cut -c"3-")" shift;;
+        "--version="*) VERSION="$(echo "$1" | cut -d"=" -f"2-")" shift;;
         "-h"|"--help") usage 0;;
-        *) break;;
+        *) usage 1;;
     esac
 done
-if [ -z "${PREFIX:="$1"}" ]
-then usage 1
+if [ -z "$DIRNAME" ]
+then
+    echo "$PATH" | tr ":" "\n" >"$TMP/path"
+    while read D
+    do
+        if [ -d "$D" -a -w "$D" ]
+        then DIRNAME="$D" break
+        fi
+    done <"$TMP/path"
 fi
-if [ -z "${VERSION:="$2"}" ]
+if [ -z "$DIRNAME" -o -z "$PREFIX" -o -z "$VERSION" ]
 then usage 1
 fi
 
-TMP="$(mktemp)"
-trap "rm -f \"$TMP\"" EXIT INT QUIT TERM
-
+# Search for the latest Substrate release by iterating over the upgrade
+# pointers until one responds 403 Forbidden (which is
+# anti-information-disclosure for 404 Not Found). The one without an upgrade
+# available is the latest release.
+echo "searching for the latest version of Substrate, starting from $VERSION" >&2
 while true
 do
     set +e
-    curl -f -o"$TMP" -s "https://src-bin.com/substrate/upgrade/$VERSION/$PREFIX"
+    curl \
+        -f \
+        -o"$TMP/upgrade" \
+        -s \
+        "https://src-bin.com/substrate/upgrade/$VERSION/$PREFIX"
     STATUS="$?"
     set -e
     case "$STATUS" in
         "0") # HTTP 200 OK
-            VERSION="$(cat "$TMP")";;
+            VERSION="$(cat "$TMP/upgrade")";; # keep upgrading
         "22") # HTTP 400+, particularly HTTP 403 Forbidden
             break;; # we've found the latest version
         *) exit "$STATUS";;
     esac
 done
+echo "found the latest version of Substrate, $VERSION" >&2
 
+# Download the latest Substrate release for this OS and architecture.
 ARCH="$(uname -m)"
 case "$ARCH" in
     "aarch64") ARCH="arm64";;
     "x86_64") ARCH="amd64";;
 esac
 OS="$(uname -s | tr "[:upper:]" "[:lower:]")"
-echo "https://src-bin.com/substrate-$VERSION-$OS-$ARCH.tar.gz" # TODO install this version
+echo "downloading <https://src-bin.com/substrate-$VERSION-$OS-$ARCH.tar.gz>" >&2
+curl \
+    -f \
+    -o"$TMP/substrate-$VERSION-$OS-$ARCH.tar.gz" \
+    -s \
+    "https://src-bin.com/substrate-$VERSION-$OS-$ARCH.tar.gz"
+
+# Untar just the binary from the latest Substrate release tarball.
+echo "untarring $TMP/substrate-$VERSION-$OS-$ARCH.tar.gz" >&2
+mkdir -p "$DIRNAME"
+tar \
+    -C"$DIRNAME" \
+    -f"$TMP/substrate-$VERSION-$OS-$ARCH.tar.gz" \
+    --strip-components 2 \
+    -x \
+    "substrate-$VERSION-$OS-$ARCH/bin/substrate"
+echo "installed $DIRNAME/substrate" >&2
+
+# Prove Substrate's installed and let it declare its version.
+"$DIRNAME/substrate" -version 2>&1
--- test-install.sh
+++ test-install.sh
@@ -1,16 +1,21 @@
-set -e -x
+set -e
 
 cd "$(dirname "$0")"
 
-TMP="$(mktemp)"
-trap "rm -f \"$TMP\"" EXIT INT QUIT TERM
+TMP="$(mktemp -d)"
+trap "rm -f -r \"$TMP\"" EXIT INT QUIT TERM
+
+set -x
 
 # When this test was written, this was the latest release, so it went directly
-# to the loop's termination condition.
-sh "install.sh" "src-bin-test1" "2022.12-eb42cd3" | tee "$TMP"
+# to the loop's termination condition. This test won't always start at loop
+# termination condition, which isn't great, but there's no way to predict the
+# necessary argument to the -v option.
+sh "install.sh" -d"$TMP/from-2022.12" -p"src-bin-test1" -v"2022.12-eb42cd3" | tee "$TMP/version"
 
-# Prior releases do eventually find their way to the same result, though.
-sh "install.sh" "src-bin-test1" "2022.11-d7cce75" | diff -u "-" "$TMP"
-sh "install.sh" "src-bin-test1" "2022.10-a9f1d56" | diff -u "-" "$TMP"
+# Ensure prior releases do eventually find their way to the same result, though.
+sh "install.sh" -d"$TMP/from-2022.11" -p"src-bin-test1" -v"2022.11-d7cce75" | diff "$TMP/version" "-"
+sh "install.sh" -d"$TMP/from-2022.10" -p"src-bin-test1" -v"2022.10-a9f1d56" | diff "$TMP/version" "-"
 
+set +x
 echo "ok" >&2
