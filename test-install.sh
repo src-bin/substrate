0e3dcc1db45de6f00fb87b0cc879e16465c88d86
Richard Crowley <r@rcrowley.org>
2022-12-13 22:49:03 -0800

    Actually, there is a way to test the latest

--- test-install.sh
+++ test-install.sh
@@ -7,15 +7,16 @@ trap "rm -f -r \"$TMP\"" EXIT INT QUIT TERM
 
 set -x
 
-# When this test was written, this was the latest release, so it went directly
-# to the loop's termination condition. This test won't always start at loop
-# termination condition, which isn't great, but there's no way to predict the
-# necessary argument to the -v option.
-sh "install.sh" -d"$TMP/from-2022.12" -p"src-bin-test1" -v"2022.12-eb42cd3" | tee "$TMP/version"
+# Ensure that running install.sh with the actual latest version installs
+# that very version.
+VERSION="$(git describe --tags HEAD | cut -d"-" -f"1")"
+COMMIT="$(git show --format=%h --no-patch "$VERSION")"
+sh "install.sh" -d"$TMP/from-latest" -p"src-bin-test1" -v"$VERSION-$COMMIT" | tee "$TMP/latest"
 
-# Ensure prior releases do eventually find their way to the same result, though.
-sh "install.sh" -d"$TMP/from-2022.11" -p"src-bin-test1" -v"2022.11-d7cce75" | diff "$TMP/version" "-"
-sh "install.sh" -d"$TMP/from-2022.10" -p"src-bin-test1" -v"2022.10-a9f1d56" | diff "$TMP/version" "-"
+# Ensure prior releases eventually find their way to the same latest version.
+sh "install.sh" -d"$TMP/from-2022.12" -p"src-bin-test1" -v"2022.12-eb42cd3" | diff "$TMP/latest" "-"
+sh "install.sh" -d"$TMP/from-2022.11" -p"src-bin-test1" -v"2022.11-d7cce75" | diff "$TMP/latest" "-"
+sh "install.sh" -d"$TMP/from-2022.10" -p"src-bin-test1" -v"2022.10-a9f1d56" | diff "$TMP/latest" "-"
 
 set +x
 echo "ok" >&2
