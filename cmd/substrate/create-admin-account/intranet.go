6be8e1f71d2325fc6dffbff8a07a0ec9b5ed1887
Richard Crowley <r@rcrowley.org>
2022-12-21 16:28:49 -0800

    Deterministic substrate-intranet.zip
    This will make it much easier to reverse-engineer the source of a
    particular source_code_hash seen on a Lambda function.

--- cmd/substrate/create-admin-account/intranet.go
+++ cmd/substrate/create-admin-account/intranet.go
@@ -3,6 +3,7 @@ package createadminaccount
 import _ "embed"
 
 //go:generate env GOARCH=arm64 GOOS=linux go build -o bootstrap ../../substrate-intranet
-//go:generate zip substrate-intranet.zip bootstrap
+//go:generate touch -t 202006100000.00 bootstrap
+//go:generate zip -X substrate-intranet.zip bootstrap
 //go:embed substrate-intranet.zip
 var SubstrateIntranetZip []byte
