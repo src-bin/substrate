3ff7acd25a0331913786320fb4f492471a04c701
Richard Crowley <r@rcrowley.org>
2023-01-03 21:08:40 -0800

    Clarify what's really going on in this test
    We're releasing at the beginning of the month now so the major hemming
    and hawing about dirty version comparisons isn't as necessary.

--- versionutil/downgrade_test.go
+++ versionutil/downgrade_test.go
@@ -4,16 +4,16 @@ import "testing"
 
 func TestVersionComparison(t *testing.T) {
 	dirtyVersion := "2022.07.13.22.14.47" // for example
-	newerVersion := "2022.07"
-	newererVersion := "2022.08"
 	olderVersion := "2022.06"
+	currentVersion := "2022.07"
+	newerVersion := "2022.08"
 	if cmp := Compare(olderVersion, dirtyVersion); cmp != Less {
 		t.Errorf("Compare(%q, %q): %v", olderVersion, dirtyVersion, cmp)
 	}
-	if cmp := Compare(dirtyVersion, newerVersion); cmp != Equal {
-		t.Errorf("Compare(%q, %q): %v", dirtyVersion, newerVersion, cmp)
+	if cmp := Compare(dirtyVersion, currentVersion); cmp != Equal {
+		t.Errorf("Compare(%q, %q): %v", dirtyVersion, currentVersion, cmp)
 	}
-	if cmp := Compare(dirtyVersion, newererVersion); cmp != Less {
-		t.Errorf("Compare(%q, %q): %v", dirtyVersion, newererVersion, cmp)
+	if cmp := Compare(dirtyVersion, newerVersion); cmp != Less {
+		t.Errorf("Compare(%q, %q): %v", dirtyVersion, newerVersion, cmp)
 	}
 }
