1af775f2bdd8bfbfce6184e11df7960fba4bb51a
Richard Crowley <r@rcrowley.org>
2023-02-13 09:53:20 -0800

    Closer: Creating the admin roles, complete policy for leaves

--- cmd/substrate/create-role/main.go
+++ cmd/substrate/create-role/main.go
@@ -8,11 +8,13 @@ import (
 	"io/fs"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/src-bin/substrate/accounts"
 	"github.com/src-bin/substrate/admin"
 	"github.com/src-bin/substrate/awscfg"
+	"github.com/src-bin/substrate/awsiam"
 	"github.com/src-bin/substrate/awsorgs"
 	"github.com/src-bin/substrate/cmdutil"
 	"github.com/src-bin/substrate/jsonutil"
@@ -20,6 +22,7 @@ import (
 	"github.com/src-bin/substrate/roles"
 	"github.com/src-bin/substrate/tagging"
 	"github.com/src-bin/substrate/ui"
+	"github.com/src-bin/substrate/users"
 	"github.com/src-bin/substrate/version"
 )
 
@@ -34,8 +37,8 @@ func Main(ctx context.Context, cfg *awscfg.Config) {
 	quiet := flag.Bool("quiet", false, "suppress status and diagnostic output")
 	humans := flag.Bool("humans", false, "allow humans with this role set in your IdP to assume this role via the Credential Factory")
 	awsServices := cmdutil.StringSliceFlag("aws-service", `allow an AWS service (by URL; e.g. "ec2.amazonaws.com") to assume role (may be repeated)`)
-	githubActions := flag.String("github-actions", "", `allow GitHub Actions to assume this role in the context of the given GitHub organization and repository (separated by a literal '/')`)
-	assumeRolePolicy := flag.String("assume-role-policy", "", "filename containing an assume-role policy to be merged into this role's final assume-role policy")
+	githubActions := cmdutil.StringSliceFlag("github-actions", `allow GitHub Actions to assume this role in the context of the given GitHub organization and repository (separated by a literal '/'; may be repeated)`)
+	assumeRolePolicyFilename := flag.String("assume-role-policy", "", "filename containing an assume-role policy to be merged into this role's final assume-role policy")
 	flag.Usage = func() {
 		ui.Print("Usage: substrate create-role [account selection flags] -role <role> [assume-role policy flags] [-quiet]")
 		ui.Print("       [account selection flags]:  -management|-special <special>")
@@ -46,8 +49,12 @@ func Main(ctx context.Context, cfg *awscfg.Config) {
 	}
 	flag.Parse()
 	version.Flag()
-	if *githubActions != "" && !strings.Contains(*githubActions, "/") {
-		ui.Fatal(`-github-actions "..." must contain a '/'`)
+	subs := make([]string, len(*githubActions))
+	for i, repo := range *githubActions {
+		if !strings.Contains(repo, "/") {
+			ui.Fatal(`-github-actions "..." must contain a '/'`)
+		}
+		subs[i] = fmt.Sprintf("repo:%s:*", repo)
 	}
 	if *quiet {
 		ui.Quiet()
@@ -56,97 +63,22 @@ func Main(ctx context.Context, cfg *awscfg.Config) {
 		ui.Fatal(`-role "..." is required`)
 	}
 
-	adminAccounts, serviceAccounts, auditAccount, deployAccount, managementAccount, networkAccount, err := accounts.Grouped(ctx, cfg)
+	adminAccounts, serviceAccounts, _, _, _, _, err := accounts.Grouped(ctx, cfg)
 	ui.Must(err)
-	_ = auditAccount                                                                                                  // TODO should we support adding roles in the audit account?
-	_, _, _, _, _, _ = adminAccounts, serviceAccounts, auditAccount, deployAccount, managementAccount, networkAccount // TODO remove and rectify
 
-	// Construct the assume-role policy for this role as it will be created in
-	// various accounts later. This governs who may assume this role.
-	// TODO accommodate multiple invocations with subtly different options.
-	policy := &policies.Document{}
-	if *humans {
-		ui.Printf("allowing humans to assume this role via your IdP")
-		canned, err := admin.CannedPrincipals(ctx, cfg, false)
-		ui.Must(err)
-		principals := &policies.Principal{AWS: []string{}}
-
-		// Create a role by the same name in admin accounts that can't
-		// (necessarily) do the same things; its purpose is to be allowed
-		// (later) to assume the roles created in the various other accounts.
-		ui.Spinf("finding or creating the %s role in your admin account(s)", *roleName)
-		for _, account := range adminAccounts {
-			// TODO create a role in every admin account that's allowed to assume roles, use Credential/Instance Factory, etc.
-			principals.AWS = append(principals.AWS, roles.ARN(aws.ToString(account.Id), *roleName))
-		}
-		ui.Stop("ok")
-
-		policy = policies.Merge(
-			policy,
-			policies.AssumeRolePolicyDocument(canned.AdminRolePrincipals), // Administrator can do anything, after all
-			policies.AssumeRolePolicyDocument(principals),
-		)
-	}
-	if len(*awsServices) > 0 {
-		ui.Printf("allowing %s to assume this role", strings.Join(*awsServices, ", "))
-		policy = policies.Merge(
-			policy,
-			policies.AssumeRolePolicyDocument(&policies.Principal{
-				Service: jsonutil.StringSlice(*awsServices),
-			}),
-		)
-	}
-	if *githubActions != "" {
-		ui.Printf("allowing GitHub Actions to assume this role on behalf of the %s repository", *githubActions)
-		policy = policies.Merge(
-			policy,
-			&policies.Document{
-				Statement: []policies.Statement{{
-					Action: []string{"sts:AssumeRoleWithWebIdentity"},
-					Condition: policies.Condition{"StringEquals": {
-						"token.actions.githubusercontent.com:sub": fmt.Sprintf("repo:%s:*", *githubActions),
-					}},
-					Principal: &policies.Principal{
-						Federated: []string{"TODO"}, // TODO create an OAuth OIDC provider per this Terraform copy-pasta
-						/*
-						   # <https://github.com/aws-actions/configure-aws-credentials>
-						   resource "aws_iam_openid_connect_provider" "github" {
-						     client_id_list  = ["sts.amazonaws.com"]
-						     thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-						     url             = "https://token.actions.githubusercontent.com"
-						   }
-						*/
-					},
-				}},
-			},
-		)
-	}
-	var filePolicy policies.Document
-	if err := jsonutil.Read(*assumeRolePolicy, &filePolicy); err == nil {
-		ui.Printf("reading additional assume-role policy statements from %s", *assumeRolePolicy)
-		policy = policies.Merge(policy, &filePolicy)
-	} else if !errors.Is(err, fs.ErrNotExist) {
-		ui.Fatal(err)
-	}
-	log.Print(jsonutil.MustString(policy))
-
-	// Create this role in every special or service account selected by the
-	// given options. Tag everything so thoroughly that `substrate roles` can
-	// reconstruct this an every other create-role invocation.
+	// Collect configs for all the accounts selected by the given options.
+	var cfgs []*awscfg.Config
 	if *management {
-		ui.Spinf("finding or creating the %s role in your management account", *roleName)
-		// TODO create the role in aws.ToString(managementAccount.Id))
-		ui.Stop("ok")
+		cfgs = append(cfgs, awscfg.Must(cfg.AssumeManagementRole(ctx, roles.OrganizationAdministrator, time.Hour)))
 	}
 	for _, special := range *specials {
-		if special == "deploy" {
-			ui.Spinf("finding or creating the %s role in your deploy account", *roleName)
-			// TODO create the role in aws.ToString(deployAccount.Id))
-			ui.Stop("ok")
-		} else if special == "network" {
-			ui.Spinf("finding or creating the %s role in your network account", *roleName)
-			// TODO create the role in aws.ToString(networkAccount.Id))
-			ui.Stop("ok")
+		switch special {
+		case accounts.Deploy:
+			cfgs = append(cfgs, awscfg.Must(cfg.AssumeSpecialRole(ctx, accounts.Deploy, roles.DeployAdministrator, time.Hour)))
+		case accounts.Network:
+			cfgs = append(cfgs, awscfg.Must(cfg.AssumeSpecialRole(ctx, accounts.Network, roles.NetworkAdministrator, time.Hour)))
+		default:
+			ui.Fatal("creating additional roles in the audit account is not supported")
 		}
 	}
 	match := func(account *awsorgs.Account) bool {
@@ -163,24 +95,139 @@ func Main(ctx context.Context, cfg *awscfg.Config) {
 	}
 	for _, account := range serviceAccounts {
 		if match(account) {
-			ui.Spinf("finding or creating the %s role in %s %+v", *roleName, aws.ToString(account.Id), account.Tags) // TODO better serialization of account
-			// TODO create the role in aws.ToString(account.Id))
-			ui.Stop("ok")
+			cfgs = append(cfgs, awscfg.Must(cfg.AssumeRole(ctx, aws.ToString(account.Id), roles.Administrator, time.Hour)))
 		}
 	}
 	if len(*numbers) > 0 {
 		for _, number := range *numbers {
-			ui.Spinf("finding or creating the %s role in account number %s", *roleName, number)
-			// TODO create the role in number
-			ui.Stop("ok")
+			cfgs = append(cfgs, awscfg.Must(cfg.AssumeRole(ctx, number, roles.Administrator, time.Hour)))
 		}
 	}
 
-	// TODO make -administrator and -auditor canned attached policies, too, plus -policy to attach a policy everywhere (except, possibly, the admin account)
+	// Every role we create needs these minimal privileges in order to use
+	// the Substrate command-line tools.
+	minimalPolicy := &policies.Document{
+		Statement: []policies.Statement{{
+			Action: []string{
+				"organizations:DescribeOrganization",
+				"sts:AssumeRole",
+			},
+			Resource: []string{"*"},
+		}},
+	}
 
-	// TODO tag the roles to make sure we can find them later
+	// If this role's for humans to use via the IdP, create a role by the same
+	// name in admin accounts that can't (necessarily) do the same things; its
+	// purpose is to be allowed (later) to assume the roles created in the
+	// various other accounts.
+	adminPrincipals := &policies.Principal{AWS: []string{}}
+	if *humans {
+		ui.Spinf("finding or creating the %s role in your admin account(s) for humans to assume via your IdP", *roleName)
+		for _, account := range adminAccounts {
+			role, err := awsiam.EnsureRoleWithPolicy(
+				ctx,
+				awscfg.Must(cfg.AssumeRole(
+					ctx,
+					aws.ToString(account.Id),
+					roles.Administrator,
+					time.Hour,
+				)),
+				*roleName,
+				policies.AssumeRolePolicyDocument(&policies.Principal{
+					AWS: []string{
+						roles.ARN(aws.ToString(account.Id), roles.Intranet),
+						users.ARN(aws.ToString(account.Id), users.CredentialFactory),
+					},
+					Service: []string{"ec2.amazonaws.com"},
+				}),
+				minimalPolicy,
+			)
+			ui.Must(err)
+			//log.Printf("%+v", role)
+			// TODO tag this role for finding and reconstructing commands later, too
+			adminPrincipals.AWS = append(adminPrincipals.AWS, role.Arn)
+		}
+		ui.Stop("ok")
+	}
 
-	// TODO tag the roles abstractly enough that we'll be able to expand to cover newly created accounts that match their parameters at creation time
+	// Create the role in each account, constructing the assume-role policy
+	// uniquely for each one because certain aspects, e.g. the GitHub Actions
+	// OAuth OIDC provider, may differ in the details from one account to
+	// another.
+	canned, err := admin.CannedPrincipals(ctx, cfg, false)
+	ui.Must(err)
+	for _, cfg := range cfgs {
+		accountId := cfg.MustAccountId(ctx)
+		ui.Printf("constructing an assume-role policy for the %s role in account number %s", *roleName, accountId)
+
+		// Construct the assume-role policy for this role as it will be created in
+		// this account. This governs who may assume this role.
+		// TODO accommodate multiple invocations with subtly different options.
+		assumeRolePolicy := &policies.Document{}
+		if *humans {
+			ui.Printf("allowing humans to assume the %s role in account number %s via admin accounts and your IdP", *roleName, accountId)
+			assumeRolePolicy = policies.Merge(
+				assumeRolePolicy,
+				policies.AssumeRolePolicyDocument(canned.AdminRolePrincipals), // Administrator can do anything, after all
+				policies.AssumeRolePolicyDocument(adminPrincipals),
+			)
+		}
+		if len(*awsServices) > 0 {
+			ui.Printf("allowing %s to assume the %s role in account number %s", strings.Join(*awsServices, ", "), *roleName, accountId)
+			assumeRolePolicy = policies.Merge(
+				assumeRolePolicy,
+				policies.AssumeRolePolicyDocument(&policies.Principal{
+					Service: jsonutil.StringSlice(*awsServices),
+				}),
+			)
+		}
+		if len(*githubActions) > 0 {
+			ui.Printf(
+				"allowing GitHub Actions to assume the %s role in account number %s on behalf of %s",
+				*roleName,
+				accountId,
+				strings.Join(*githubActions, ", "),
+			)
+			arn, err := awsiam.EnsureOpenIDConnectProvider(
+				ctx,
+				cfg,
+				[]string{"sts.amazonaws.com"},
+				[]string{"6938fd4d98bab03faadb97b34396831e3780aea1"},
+				"https://token.actions.githubusercontent.com",
+			)
+			ui.Must(err)
+			assumeRolePolicy = policies.Merge(
+				assumeRolePolicy,
+				&policies.Document{
+					Statement: []policies.Statement{{
+						Action: []string{"sts:AssumeRoleWithWebIdentity"},
+						Condition: policies.Condition{"StringEquals": {
+							"token.actions.githubusercontent.com:sub": subs,
+						}},
+						Principal: &policies.Principal{
+							Federated: []string{arn},
+						},
+					}},
+				},
+			)
+		}
+		var filePolicy policies.Document
+		if err := jsonutil.Read(*assumeRolePolicyFilename, &filePolicy); err == nil {
+			ui.Printf("reading additional assume-role policy statements from %s", *assumeRolePolicyFilename)
+			assumeRolePolicy = policies.Merge(assumeRolePolicy, &filePolicy)
+		} else if !errors.Is(err, fs.ErrNotExist) {
+			ui.Fatal(err)
+		}
+		log.Print(jsonutil.MustString(assumeRolePolicy))
+
+		ui.Spinf("finding or creating the %s role in account number %s", *roleName, accountId)
+		// TODO create the role with this assume-role policy
+		// TODO tag the roles to make sure we can find them later
+		// TODO tag the roles abstractly enough that we'll be able to expand to cover newly created accounts that match their parameters at creation time
+		ui.Stopf("ok")
+
+		// TODO -administrator and -auditor canned attached policies, too, plus -policy to attach a policy everywhere (except, possibly, admin accounts)
+	}
 
 }
 
