5de9d42d2076ac94b16d704b0ad1b8a2fab23417
Richard Crowley <r@rcrowley.org>
2023-02-20 21:02:13 -0800

    Protect ourselves from a race in S3 bucket creation

--- awss3/buckets.go
+++ awss3/buckets.go
@@ -15,6 +15,7 @@ import (
 )
 
 const (
+	AllAccessDisabled       = "AllAccessDisabled"
 	BucketAlreadyOwnedByYou = "BucketAlreadyOwnedByYou"
 	Enabled                 = "Enabled"
 	NotSignedUp             = "NotSignedUp"
@@ -25,10 +26,10 @@ func EnsureBucket(
 	cfg *awscfg.Config,
 	name, region string,
 	doc *policies.Document,
-) error {
+) (err error) {
 
 	for {
-		err := createBucket(ctx, cfg, name, region)
+		err = createBucket(ctx, cfg, name, region)
 		if awsutil.ErrorCodeIs(err, NotSignedUp) {
 			time.Sleep(1e9) // TODO exponential backoff
 			continue
@@ -37,32 +38,44 @@ func EnsureBucket(
 			err = nil
 		}
 		if err != nil {
-			return err
+			return
 		}
 		break
 	}
 
 	client := cfg.S3()
 
-	if _, err := client.PutBucketAcl(ctx, &s3.PutBucketAclInput{
-		ACL:    types.BucketCannedACLPrivate, // the default but let's be explicit
-		Bucket: aws.String(name),
-	}); err != nil {
-		return err
+	// A customer once experienced a race here wherein the newly created S3
+	// bucket wasn't immediately ready to have ACLs, etc. set and so failed.
+	// The error was very peculiar but it's fortunately so nonsensical that
+	// we can safely assume that it won't happen for any real reasons and
+	// instead can be a clear sign of this race.
+	for {
+		_, err = client.PutBucketAcl(ctx, &s3.PutBucketAclInput{
+			ACL:    types.BucketCannedACLPrivate, // the default but let's be explicit
+			Bucket: aws.String(name),
+		})
+		if !awsutil.ErrorCodeIs(err, AllAccessDisabled) {
+			break
+		}
+		time.Sleep(1e9) // TODO exponential backoff
+	}
+	if err != nil {
+		return
 	}
 
 	docJSON, err := doc.Marshal()
 	if err != nil {
-		return err
+		return
 	}
-	if _, err := client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
+	if _, err = client.PutBucketPolicy(ctx, &s3.PutBucketPolicyInput{
 		Bucket: aws.String(name),
 		Policy: aws.String(docJSON),
 	}); err != nil {
-		return err
+		return
 	}
 
-	if _, err := client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
+	if _, err = client.PutBucketTagging(ctx, &s3.PutBucketTaggingInput{
 		Bucket: aws.String(name),
 		Tagging: &types.Tagging{
 			TagSet: []types.Tag{
@@ -71,20 +84,20 @@ func EnsureBucket(
 			},
 		},
 	}); err != nil {
-		return err
+		return
 	}
 
 	// TODO maybe make this optional or integrate it with a lifecycle policy on old versions?
-	if _, err := client.PutBucketVersioning(ctx, &s3.PutBucketVersioningInput{
+	if _, err = client.PutBucketVersioning(ctx, &s3.PutBucketVersioningInput{
 		Bucket: aws.String(name),
 		VersioningConfiguration: &types.VersioningConfiguration{
 			Status: types.BucketVersioningStatusEnabled,
 		},
 	}); err != nil {
-		return err
+		return
 	}
 
-	if _, err := client.PutPublicAccessBlock(ctx, &s3.PutPublicAccessBlockInput{
+	if _, err = client.PutPublicAccessBlock(ctx, &s3.PutPublicAccessBlockInput{
 		Bucket: aws.String(name),
 		PublicAccessBlockConfiguration: &types.PublicAccessBlockConfiguration{
 			BlockPublicAcls:       true,
@@ -93,10 +106,10 @@ func EnsureBucket(
 			RestrictPublicBuckets: true,
 		},
 	}); err != nil {
-		return err
+		return
 	}
 
-	return nil
+	return
 }
 
 func createBucket(ctx context.Context, cfg *awscfg.Config, name, region string) (err error) {
