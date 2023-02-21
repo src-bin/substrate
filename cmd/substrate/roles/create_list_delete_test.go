d2405453e82bebb464ef24852821a2e767ab7039
Richard Crowley <r@rcrowley.org>
2023-02-21 09:18:42 -0800

    Tests covering a few more role selections

--- cmd/substrate/roles/create_list_delete_test.go
+++ cmd/substrate/roles/create_list_delete_test.go
@@ -3,6 +3,7 @@ package roles
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/src-bin/substrate/awscfg"
 	"github.com/src-bin/substrate/awscfg/testawscfg"
@@ -11,26 +12,102 @@ import (
 	createrole "github.com/src-bin/substrate/cmd/substrate/create-role"
 	deleterole "github.com/src-bin/substrate/cmd/substrate/delete-role"
 	"github.com/src-bin/substrate/cmdutil"
+	"github.com/src-bin/substrate/naming"
 	"github.com/src-bin/substrate/roles"
 )
 
-func TestCreateAndDeleteRole(t *testing.T) {
+func TestCreateAndDeleteHumanRole(t *testing.T) {
+	const (
+		roleName    = "Foo"
+		domain      = "foo"
+		environment = "staging"
+		quality     = "default"
+	)
 	defer cmdutil.RestoreArgs()
 	ctx := context.Background()
 	cfg := testawscfg.Test1(roles.Administrator)
-	const roleName = "Foo"
+	fooCfg := awscfg.Must(cfg.AssumeServiceRole(
+		ctx,
+		domain, environment, quality,
+		roles.Administrator,
+		time.Hour,
+	))
 
-	ensureRoleDoesNotExist(t, ctx, cfg, roleName)
+	//ensureRoleDoesNotExist(t, ctx, cfg, roleName)
+	//ensureRoleDoesNotExist(t, ctx, fooCfg, roleName)
 
-	cmdutil.OverrideArgs("-domain", "foo", "-humans", "-role", roleName)
+	cmdutil.OverrideArgs("-domain", domain, "-humans", "-role", roleName)
 	createrole.Main(ctx, cfg)
 
-	ensureRoleExists(t, ctx, cfg, roleName)
+	ensureRoleExists(t, ctx, cfg, roleName)    // because -humans
+	ensureRoleExists(t, ctx, fooCfg, roleName) // because -domain
+	// TODO test that this role does not exist in any other accounts
 
 	cmdutil.OverrideArgs("-delete", "-role", roleName)
 	deleterole.Main(ctx, cfg)
 
 	ensureRoleDoesNotExist(t, ctx, cfg, roleName)
+	ensureRoleDoesNotExist(t, ctx, fooCfg, roleName)
+}
+
+func TestCreateAndDeleteManagementRole(t *testing.T) {
+	const roleName = "Mgmt"
+	defer cmdutil.RestoreArgs()
+	ctx := context.Background()
+	cfg := testawscfg.Test1(roles.Administrator)
+	mgmtCfg := awscfg.Must(cfg.AssumeManagementRole(
+		ctx,
+		roles.OrganizationAdministrator,
+		time.Hour,
+	))
+
+	cmdutil.OverrideArgs("-management", "-role", roleName)
+	createrole.Main(ctx, cfg)
+
+	ensureRoleDoesNotExist(t, ctx, cfg, roleName) // because no -humans
+	ensureRoleExists(t, ctx, mgmtCfg, roleName)   // because -management
+	// TODO test that this role does not exist in any other accounts
+
+	cmdutil.OverrideArgs("-delete", "-role", roleName)
+	deleterole.Main(ctx, cfg)
+
+	ensureRoleDoesNotExist(t, ctx, mgmtCfg, roleName)
+}
+
+func TestCreateAndDeleteSpecialRole(t *testing.T) {
+	const roleName = "Special"
+	defer cmdutil.RestoreArgs()
+	ctx := context.Background()
+	cfg := testawscfg.Test1(roles.Administrator)
+	deployCfg := awscfg.Must(cfg.AssumeSpecialRole(
+		ctx,
+		naming.Deploy,
+		roles.DeployAdministrator,
+		time.Hour,
+	))
+	networkCfg := awscfg.Must(cfg.AssumeSpecialRole(
+		ctx,
+		naming.Network,
+		roles.NetworkAdministrator,
+		time.Hour,
+	))
+
+	ensureRoleDoesNotExist(t, ctx, deployCfg, roleName)
+	ensureRoleDoesNotExist(t, ctx, networkCfg, roleName)
+
+	cmdutil.OverrideArgs("-role", roleName, "-special", naming.Deploy, "-special", naming.Network)
+	createrole.Main(ctx, cfg)
+
+	ensureRoleDoesNotExist(t, ctx, cfg, roleName)  // because no -humans
+	ensureRoleExists(t, ctx, deployCfg, roleName)  // because -special deploy
+	ensureRoleExists(t, ctx, networkCfg, roleName) // because -special network
+	// TODO test that this role does not exist in any other accounts
+
+	cmdutil.OverrideArgs("-delete", "-role", roleName)
+	deleterole.Main(ctx, cfg)
+
+	ensureRoleDoesNotExist(t, ctx, deployCfg, roleName)
+	ensureRoleDoesNotExist(t, ctx, networkCfg, roleName)
 }
 
 func ensureRoleDoesNotExist(t *testing.T, ctx context.Context, cfg *awscfg.Config, roleName string) {
