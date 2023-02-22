8b0402d97fe00e42aed10a19e92f74428b311423
Richard Crowley <r@rcrowley.org>
2023-02-22 10:49:58 -0800

    Failing tests showing why we need -all-domains, etc.
    If you run with just e.g. -management, the sensible expectation is that
    it only creates the role in the management account. However, without any
    -domain, -environment, and -quality arguments, it assumes it should
    create the role in all serivce accounts. That makes sense when there's
    not also a -management or similar but is confusing when there is. The
    result is that there's no way to create a role _just_ in the management
    or one or more special accounts.
    
    These tests show this clearly and the solution (coming soon) will be to
    require folks to explicitly opt into roles being created in all service
    accounts by providing some combination of -all-domains,
    -all-environments, and -all-qualities.

--- cmd/substrate/roles/create_list_delete_test.go
+++ cmd/substrate/roles/create_list_delete_test.go
@@ -5,21 +5,26 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/src-bin/substrate/accounts"
 	"github.com/src-bin/substrate/awscfg"
 	"github.com/src-bin/substrate/awscfg/testawscfg"
 	"github.com/src-bin/substrate/awsiam"
+	"github.com/src-bin/substrate/awsorgs"
 	"github.com/src-bin/substrate/awsutil"
 	createrole "github.com/src-bin/substrate/cmd/substrate/create-role"
 	deleterole "github.com/src-bin/substrate/cmd/substrate/delete-role"
 	"github.com/src-bin/substrate/cmdutil"
 	"github.com/src-bin/substrate/naming"
 	"github.com/src-bin/substrate/roles"
+	"github.com/src-bin/substrate/ui"
 )
 
 func TestCreateAndDeleteHumanRole(t *testing.T) {
 	const (
 		roleName    = "Foo"
 		domain      = "foo"
+		otherDomain = "bar"
 		environment = "staging"
 		quality     = "default"
 	)
@@ -40,8 +45,13 @@ func TestCreateAndDeleteHumanRole(t *testing.T) {
 	createrole.Main(ctx, cfg)
 
 	testRole(t, ctx, cfg, roleName, testExists)    // because -humans
-	testRole(t, ctx, fooCfg, roleName, testExists) // because -domain
-	// TODO test that this role does not exist in any other accounts
+	testRole(t, ctx, fooCfg, roleName, testExists) // because -domain "foo"
+	testRole(t, ctx, awscfg.Must(cfg.AssumeServiceRole(
+		ctx,
+		otherDomain, environment, quality,
+		roles.Administrator,
+		time.Hour,
+	)), roleName, testNotExists) // because -domain "foo" not -domain "bar"
 
 	cmdutil.OverrideArgs("-delete", "-role", roleName)
 	deleterole.Main(ctx, cfg)
@@ -60,20 +70,56 @@ func TestCreateAndDeleteManagementRole(t *testing.T) {
 		roles.OrganizationAdministrator,
 		time.Hour,
 	))
+	_, serviceAccounts, _, deployAccount, _, networkAccount, err := accounts.Grouped(ctx, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	//testRole(t, ctx, mgmtCfg, roleName, testNotExists)
 
 	cmdutil.OverrideArgs("-management", "-role", roleName)
 	createrole.Main(ctx, cfg)
 
-	testRole(t, ctx, cfg, roleName, testNotExists)  // because no -humans
-	testRole(t, ctx, mgmtCfg, roleName, testExists) // because -management
-	// TODO test that this role does not exist in any other accounts
+	testRole(t, ctx, cfg, roleName, testNotExists)                            // because no -humans
+	testRole(t, ctx, mgmtCfg, roleName, testExists)                           // because -management
+	testRoleInAccounts(t, ctx, cfg, serviceAccounts, roleName, testNotExists) // because -management and no -domain, -environment, or -quality
+	testRoleInAccounts(t, ctx, cfg, []*awsorgs.Account{
+		deployAccount,  // because no -special "deploy"
+		networkAccount, // because no -special "network"
+	}, roleName, testNotExists)
 
 	cmdutil.OverrideArgs("-delete", "-role", roleName)
 	deleterole.Main(ctx, cfg)
 
 	testRole(t, ctx, mgmtCfg, roleName, testNotExists)
+}
+
+func TestCreateAndDeleteServiceRole(t *testing.T) {
+	const roleName = "FooBarBaz"
+	defer cmdutil.RestoreArgs()
+	ctx := context.Background()
+	cfg := testawscfg.Test1(roles.Administrator)
+	_, serviceAccounts, _, deployAccount, managementAccount, networkAccount, err := accounts.Grouped(ctx, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cmdutil.OverrideArgs("-role", roleName)
+	createrole.Main(ctx, cfg)
+
+	testRole(t, ctx, cfg, roleName, testNotExists)                         // because no -humans
+	testRoleInAccounts(t, ctx, cfg, serviceAccounts, roleName, testExists) // because defaults
+	testRoleInAccounts(t, ctx, cfg, []*awsorgs.Account{
+		deployAccount,     // because no -special "deploy"
+		managementAccount, // because no -management or -special
+		networkAccount,    // because no -special "network"
+	}, roleName, testNotExists)
+
+	cmdutil.OverrideArgs("-delete", "-role", roleName)
+	deleterole.Main(ctx, cfg)
+
+	testRole(t, ctx, cfg, roleName, testNotExists)
+	testRoleInAccounts(t, ctx, cfg, serviceAccounts, roleName, testNotExists)
 }
 
 func TestCreateAndDeleteSpecialRole(t *testing.T) {
@@ -93,6 +139,10 @@ func TestCreateAndDeleteSpecialRole(t *testing.T) {
 		roles.NetworkAdministrator,
 		time.Hour,
 	))
+	_, serviceAccounts, _, _, _, _, err := accounts.Grouped(ctx, cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
 
 	//testRole(t, ctx, deployCfg, roleName, testNotExists)
 	//testRole(t, ctx, networkCfg, roleName, testNotExists)
@@ -100,16 +150,20 @@ func TestCreateAndDeleteSpecialRole(t *testing.T) {
 	cmdutil.OverrideArgs("-role", roleName, "-special", naming.Deploy, "-special", naming.Network)
 	createrole.Main(ctx, cfg)
 
-	testRole(t, ctx, cfg, roleName, testNotExists)     // because no -humans
-	testRole(t, ctx, deployCfg, roleName, testExists)  // because -special deploy
-	testRole(t, ctx, networkCfg, roleName, testExists) // because -special network
-	// TODO test that this role does not exist in any other accounts
+	testRole(t, ctx, cfg, roleName, testNotExists)                            // because no -humans
+	testRole(t, ctx, deployCfg, roleName, testExists)                         // because -special deploy
+	testRole(t, ctx, networkCfg, roleName, testExists)                        // because -special network
+	testRoleInAccounts(t, ctx, cfg, serviceAccounts, roleName, testNotExists) // because -special and no -domain, -environment, or -quality
 
 	cmdutil.OverrideArgs("-delete", "-role", roleName)
 	deleterole.Main(ctx, cfg)
 
 	testRole(t, ctx, deployCfg, roleName, testNotExists)
 	testRole(t, ctx, networkCfg, roleName, testNotExists)
+}
+
+func init() {
+	ui.Quiet()
 }
 
 const (
@@ -127,15 +181,32 @@ func testRole(
 	role, err := awsiam.GetRole(ctx, cfg, roleName)
 	if test == testExists {
 		if err != nil {
-			t.Fatalf("expected nil but got %v", err)
+			t.Errorf("expected nil in account %s but got %v", cfg.MustAccountId(ctx), err)
 		}
 	} else {
 		if err == nil {
-			t.Fatalf("expected NoSuchEntity but got %+v", role)
-		}
-		if !awsutil.ErrorCodeIs(err, awsiam.NoSuchEntity) {
-			t.Fatalf("expected NoSuchEntity but got %v", err)
+			t.Errorf("expected NoSuchEntity in account %s but got %s", cfg.MustAccountId(ctx), role.Arn)
+		} else if !awsutil.ErrorCodeIs(err, awsiam.NoSuchEntity) {
+			t.Errorf("expected NoSuchEntity in account %s but got %v", cfg.MustAccountId(ctx), err)
 		}
 	}
-	t.Log(role)
+	//t.Log(role)
+}
+
+func testRoleInAccounts(
+	t *testing.T,
+	ctx context.Context,
+	cfg *awscfg.Config,
+	accounts []*awsorgs.Account,
+	roleName string,
+	test bool,
+) {
+	for _, account := range accounts {
+		testRole(t, ctx, awscfg.Must(cfg.AssumeRole(
+			ctx,
+			aws.ToString(account.Id),
+			account.AdministratorRoleName(),
+			time.Hour,
+		)), roleName, test)
+	}
 }
