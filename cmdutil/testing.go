c9add90014ce78c48b2122610a8863e801968d85
Richard Crowley <r@rcrowley.org>
2023-02-25 22:54:39 -0800

    Comments

--- cmdutil/testing.go
+++ cmdutil/testing.go
@@ -5,11 +5,16 @@ import (
 	"os"
 )
 
+// OverrideArgs provides a way to test main functions that use package-level
+// flag functions to parse command-line arguments. It must be called _before_
+// calling and package-level flag functions.
 func OverrideArgs(args ...string) {
 	os.Args = append(os.Args[0:1], args...)
 	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
 }
 
+// RestoreArgs puts everything back where it was. It should be called in a
+// defer statement at the beginning of a test or benchmark function.
 func RestoreArgs() {
 	os.Args = append(os.Args[0:1], originalArgs...)
 }
