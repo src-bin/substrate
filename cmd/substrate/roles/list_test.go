910d2bc1f999f8399fd4aab168bd778ce1fe782d
Richard Crowley <r@rcrowley.org>
2023-02-26 23:52:36 -0800

    Test for `substrate roles -format json`

--- cmd/substrate/roles/list_test.go
+++ cmd/substrate/roles/list_test.go
@@ -1,7 +1,9 @@
 package roles
 
 import (
+	"bytes"
 	"context"
+	"os"
 	"testing"
 	"time"
 
@@ -10,6 +12,7 @@ import (
 	createrole "github.com/src-bin/substrate/cmd/substrate/create-role"
 	deleterole "github.com/src-bin/substrate/cmd/substrate/delete-role"
 	"github.com/src-bin/substrate/cmdutil"
+	"github.com/src-bin/substrate/fileutil"
 	"github.com/src-bin/substrate/roles"
 )
 
@@ -48,12 +51,137 @@ func TestEverything(t *testing.T) {
 		time.Hour,
 	)), roleName, testNotExists) // because no -domain "baz"
 
+	var err error
+	os.Stdout, err = os.CreateTemp("", "TestEverything-*.stdout")
+	if err != nil {
+		t.Fatal(err)
+	}
+	pathname := os.Stdout.Name()
+	defer os.Remove(pathname)
+
 	cmdutil.OverrideArgs("-format", "json")
 	Main(ctx, cfg)
-	// TODO figure out how to make assertions on its output
+
+	os.Stdout = stdout
+	actual, err := fileutil.ReadFile(pathname)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := []byte(`[
+	{
+		"RoleName": "Everything",
+		"AccountSelection": {
+			"AllDomains": false,
+			"Domains": [
+				"bar",
+				"foo"
+			],
+			"AllEnvironments": false,
+			"Environments": [
+				"staging"
+			],
+			"AllQualities": true,
+			"Qualities": null,
+			"Admin": true,
+			"Management": true,
+			"Specials": [
+				"deploy",
+				"network"
+			],
+			"Numbers": null
+		},
+		"AssumeRolePolicy": {
+			"Humans": true,
+			"AWSServices": [
+				"ec2.amazonaws.com",
+				"lambda.amazonaws.com"
+			],
+			"GitHubActions": null,
+			"Filenames": [
+				"policies/FooBar.assume-role-policy.json"
+			]
+		},
+		"PolicyAttachments": {
+			"Administrator": true,
+			"ReadOnly": false,
+			"ARNs": [
+				"arn:aws:iam::aws:policy/job-function/Billing"
+			],
+			"Filenames": [
+				"policies/FooBar.policy.json"
+			]
+		},
+		"RoleARNs": [
+			"arn:aws:iam::306228446141:role/Everything",
+			"arn:aws:iam::488246444926:role/Everything",
+			"arn:aws:iam::617136454425:role/Everything",
+			"arn:aws:iam::716893237583:role/Everything",
+			"arn:aws:iam::765683503745:role/Everything",
+			"arn:aws:iam::903998760555:role/Everything"
+		]
+	},
+	{
+		"RoleName": "FooBar",
+		"AccountSelection": {
+			"AllDomains": false,
+			"Domains": [
+				"bar",
+				"foo"
+			],
+			"AllEnvironments": false,
+			"Environments": [
+				"staging"
+			],
+			"AllQualities": true,
+			"Qualities": null,
+			"Admin": true,
+			"Management": true,
+			"Specials": [
+				"deploy",
+				"network"
+			],
+			"Numbers": null
+		},
+		"AssumeRolePolicy": {
+			"Humans": true,
+			"AWSServices": [
+				"ec2.amazonaws.com",
+				"lambda.amazonaws.com"
+			],
+			"GitHubActions": null,
+			"Filenames": [
+				"policies/FooBar.assume-role-policy.json"
+			]
+		},
+		"PolicyAttachments": {
+			"Administrator": true,
+			"ReadOnly": false,
+			"ARNs": [
+				"arn:aws:iam::aws:policy/job-function/Billing"
+			],
+			"Filenames": [
+				"policies/FooBar.policy.json"
+			]
+		},
+		"RoleARNs": [
+			"arn:aws:iam::306228446141:role/FooBar",
+			"arn:aws:iam::488246444926:role/FooBar",
+			"arn:aws:iam::617136454425:role/FooBar",
+			"arn:aws:iam::716893237583:role/FooBar",
+			"arn:aws:iam::765683503745:role/FooBar",
+			"arn:aws:iam::903998760555:role/FooBar"
+		]
+	}
+]
+`)
+	if !bytes.Equal(actual, expected) {
+		t.Error("`substrate roles -format json` output is wrong") // TODO pass actual and expected to diff(1)
+	}
 
 	cmdutil.OverrideArgs("-delete", "-role", roleName)
 	deleterole.Main(ctx, cfg)
 
 	testRole(t, ctx, cfg, roleName, testNotExists)
 }
+
+var stdout = os.Stdout
