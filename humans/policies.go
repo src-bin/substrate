cee3c7cf3776d8ab515957e014e0f6f771e46c57
Richard Crowley <r@rcrowley.org>
2023-08-05 22:36:45 -0700

    Add a canned policy for just the Intranet's principals
    This includes the new Substrate role and user which the Intranet will
    use as well as EC2, which of course the Instance Factory uses.

--- humans/policies.go
+++ humans/policies.go
@@ -10,7 +10,7 @@ import (
 	"github.com/src-bin/substrate/users"
 )
 
-// AdministratorAssumeRolePolicy constructs and returns an assume role policy
+// AdministratorAssumeRolePolicy constructs and returns an assume-role policy
 // for the Administrator role in any account. The given *Config must be for the
 // Substrate account or the management account.
 func AdministratorAssumeRolePolicy(ctx context.Context, cfg *awscfg.Config) (*policies.Document, error) {
@@ -43,7 +43,7 @@ func AdministratorAssumeRolePolicy(ctx context.Context, cfg *awscfg.Config) (*po
 	), nil
 }
 
-// AuditorAssumeRolePolicy constructs and returns an assume role policy for
+// AuditorAssumeRolePolicy constructs and returns an assume-role policy for
 // the Auditor role in any account. The given *Config must be for the
 // Substrate account or the management account.
 func AuditorAssumeRolePolicy(ctx context.Context, cfg *awscfg.Config) (*policies.Document, error) {
@@ -79,5 +79,25 @@ func AuditorAssumeRolePolicy(ctx context.Context, cfg *awscfg.Config) (*policies
 		}),
 		extraAdministrator,
 		extraAuditor,
+	), nil
+}
+
+// IntranetAssumeRolePolicy constructs and returns an assume-role policy for
+// the Intranet to use in order to provide 12-hour sessions. The given *Config
+// must be for the Substrate account or the management account.
+func IntranetAssumeRolePolicy(ctx context.Context, cfg *awscfg.Config) (*policies.Document, error) {
+	substrateAccount, err := cfg.FindSubstrateAccount(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return policies.Merge(
+		policies.AssumeRolePolicyDocument(&policies.Principal{
+			AWS: []string{
+				roles.ARN(aws.ToString(substrateAccount.Id), roles.Substrate),
+				users.ARN(aws.ToString(substrateAccount.Id), users.Substrate),
+			},
+			Service: []string{"ec2.amazonaws.com"},
+		}),
 	), nil
 }
