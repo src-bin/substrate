4ca72f8dd11d021997b1443f590fd191d2359f61
Richard Crowley <r@rcrowley.org>
2023-08-09 16:41:38 -0700

    12-hour AWS Console sessions at last

--- cmd/substrate-intranet/accounts.go
+++ cmd/substrate-intranet/accounts.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
 	"time"
 
 	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/src-bin/substrate/accounts"
 	"github.com/src-bin/substrate/authorizerutil"
 	"github.com/src-bin/substrate/awscfg"
+	"github.com/src-bin/substrate/awsiam"
 	"github.com/src-bin/substrate/awsorgs"
 	"github.com/src-bin/substrate/federation"
 	"github.com/src-bin/substrate/lambdautil"
@@ -33,19 +36,11 @@ func accountsHandler(
 	accountId := event.QueryStringParameters["number"]
 	roleName := event.QueryStringParameters["role"]
 	if accountId != "" && roleName != "" {
-
-		/*
-			var cfg12h *awscfg.Config
-			if cfg12h, err = awsiam.AllDayConfig(ctx, cfg); err != nil {
-				return lambdautil.ErrorResponse(err)
-			}
-		*/
-
-		// XXX uh oh, we can't naively do this because first we have to assume our role here and then we have to assume the requested role
+		var cfg12h, credsCfg, credsCfg12h, userCfg *awscfg.Config
 
 		// We have to start from the user's configured starting point so that
 		// all questions of authorization are deferred to AWS.
-		if cfg, err = cfg.AssumeRole(
+		if userCfg, err = cfg.AssumeRole(
 			ctx,
 			event.RequestContext.AccountID,
 			event.RequestContext.Authorizer[authorizerutil.RoleName].(string),
@@ -57,15 +52,28 @@ func accountsHandler(
 		roleArn := roles.ARN(accountId, roleName)
 		cfg.Telemetry().SetFinalAccountId(accountId)
 		cfg.Telemetry().SetFinalRoleName(roleArn)
-		if cfg, err = cfg.AssumeRole(
-			ctx,
-			accountId,
-			roleName,
-			time.Hour,
-		); err != nil {
+
+		// First, assume the role directly to ensure it's really authorized.
+		if credsCfg, err = userCfg.AssumeRole(ctx, accountId, roleName, time.Hour); err != nil {
 			return lambdautil.ErrorResponse(err)
 		}
-		creds, err := cfg.Retrieve(ctx)
+
+		// If that worked, try to assume the role from an IAM user to get
+		// 12-hour credentials.
+		if cfg12h, err = awsiam.AllDayConfig(ctx, cfg); err != nil {
+			return lambdautil.ErrorResponse(err)
+		}
+		if credsCfg12h, err = cfg12h.AssumeRole(ctx, accountId, roleName, 12*time.Hour); err != nil {
+			log.Print(err) // continue because this is optional
+		}
+
+		// Finally, fetch credentials. 12-hour if we can, 1-hour if we have to.
+		var creds aws.Credentials
+		if credsCfg12h != nil {
+			creds, err = credsCfg12h.Retrieve(ctx)
+		} else {
+			creds, err = credsCfg.Retrieve(ctx)
+		}
 		if err != nil {
 			return lambdautil.ErrorResponse(err)
 		}
