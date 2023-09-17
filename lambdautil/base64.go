acca7994424d2d358fa5d80739049f7a1d1cf3f9
Richard Crowley <r@rcrowley.org>
2023-09-16 22:03:57 -0700

    Parallel functions for API Gateway v2

--- lambdautil/base64.go
+++ lambdautil/base64.go
@@ -18,8 +18,27 @@ func EventBody(event *events.APIGatewayProxyRequest) (string, error) {
 	return event.Body, nil
 }
 
+func EventBody2(event *events.APIGatewayV2HTTPRequest) (string, error) {
+	if event.IsBase64Encoded {
+		b, err := base64.StdEncoding.DecodeString(event.Body)
+		if err != nil {
+			return "", err
+		}
+		return string(b), nil
+	}
+	return event.Body, nil
+}
+
 func EventBodyBuffer(event *events.APIGatewayProxyRequest) (*bytes.Buffer, error) {
 	body, err := EventBody(event)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBufferString(body), nil
+}
+
+func EventBodyBuffer2(event *events.APIGatewayV2HTTPRequest) (*bytes.Buffer, error) {
+	body, err := EventBody2(event)
 	if err != nil {
 		return nil, err
 	}
