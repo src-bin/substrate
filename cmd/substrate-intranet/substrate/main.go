f82c5588f4a35518f7bbc585d3a0145ce8ab6fb9
Richard Crowley <r@rcrowley.org>
2023-09-18 22:13:06 -0700

    API Gateway v2 version of /substrate

--- cmd/substrate-intranet/substrate/main.go
+++ cmd/substrate-intranet/substrate/main.go
@@ -2,6 +2,7 @@ package substrate
 
 import (
 	"context"
+	_ "embed"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -13,8 +14,6 @@ import (
 	"github.com/src-bin/substrate/version"
 	"github.com/src-bin/substrate/versionutil"
 )
-
-//go:generate go run ../../../tools/template/main.go -name mainTemplate main.html
 
 func Main(
 	ctx context.Context,
@@ -39,7 +38,7 @@ func Main(
 		versionutil.DownloadURL(v, "linux", "arm64"),
 	}
 
-	body, err := lambdautil.RenderHTML(mainTemplate(), struct {
+	body, err := lambdautil.RenderHTML(html, struct {
 		Version, UpgradeVersion string
 		DownloadURLs            []*url.URL
 	}{
@@ -57,3 +56,48 @@ func Main(
 		StatusCode: http.StatusOK,
 	}, nil
 }
+
+func Main2(
+	ctx context.Context,
+	cfg *awscfg.Config,
+	oc *oauthoidc.Client,
+	event *events.APIGatewayV2HTTPRequest,
+) (*events.APIGatewayV2HTTPResponse, error) {
+
+	upgradeVersion, _, err := versionutil.CheckForUpgrade()
+	if err != nil {
+		return nil, err
+	}
+
+	v := upgradeVersion
+	if v == "" {
+		v = fmt.Sprintf("%s-%s", version.Version, version.Commit)
+	}
+	downloadURLs := []*url.URL{
+		versionutil.DownloadURL(v, "darwin", "amd64"),
+		versionutil.DownloadURL(v, "darwin", "arm64"),
+		versionutil.DownloadURL(v, "linux", "amd64"),
+		versionutil.DownloadURL(v, "linux", "arm64"),
+	}
+
+	body, err := lambdautil.RenderHTML(html, struct {
+		Version, UpgradeVersion string
+		DownloadURLs            []*url.URL
+	}{
+		Version:        fmt.Sprintf("%s-%s", version.Version, version.Commit),
+		UpgradeVersion: upgradeVersion,
+		DownloadURLs:   downloadURLs,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &events.APIGatewayV2HTTPResponse{
+		Body:       body,
+		Headers:    map[string]string{"Content-Type": "text/html; charset=utf-8"},
+		StatusCode: http.StatusOK,
+	}, nil
+}
+
+//go:embed substrate.html
+var html string
