78b78da97f6df3351ed4257ffa409eb574ec0948
Richard Crowley <r@rcrowley.org>
2023-09-18 20:45:16 -0700

    Intranet Index for API Gateway v2

--- cmd/substrate-intranet/index/main.go
+++ cmd/substrate-intranet/index/main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/apigateway"
+	"github.com/src-bin/substrate/awsapigatewayv2"
 	"github.com/src-bin/substrate/awscfg"
 	"github.com/src-bin/substrate/lambdautil"
 	"github.com/src-bin/substrate/oauthoidc"
@@ -104,24 +105,23 @@ func Main2(
 		debug += string(b) + "\n" + strings.Join(os.Environ(), "\n") + "\n"
 	}
 
-	out, err := cfg.APIGateway().GetResources(ctx, &apigateway.GetResourcesInput{
-		Limit:     aws.Int32(500),
-		RestApiId: aws.String(event.RequestContext.APIID),
-	})
+	routes, err := awsapigatewayv2.GetRoutes(ctx, cfg, event.RequestContext.APIID)
 	if err != nil {
 		return nil, err
 	}
 	paths := make([]string, 0)
-	for _, item := range out.Items {
-		path := aws.ToString(item.Path)
+	for _, route := range routes {
+		path := aws.ToString(route.RouteKey)
+		if strings.HasPrefix(path, "GET ") {
+			path = path[4:]
+		} else {
+			continue // unlists "$default", which is most of the Substrate-managed Intranet
+		}
+		if path == "/login" {
+			continue // unlists the rest of the Substrate-managed Intranet
+		}
 		if strings.Contains(path, "{") {
-			continue
-		}
-		if strings.Count(path, "/") >= 3 {
-			continue
-		}
-		if i := sort.SearchStrings(unlistedPaths, path); i < len(unlistedPaths) && unlistedPaths[i] == path {
-			continue
+			continue // unlists parameterized paths we can't meaningfully link to
 		}
 		paths = append(paths, path)
 	}
