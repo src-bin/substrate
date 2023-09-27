ffa8e2a5f9297f4ea0b788bbc3836c461675654c
Richard Crowley <r@rcrowley.org>
2023-09-27 01:31:45 -0700

    Useful giant API Gateway v2 logs

--- awsapigatewayv2/apis.go
+++ awsapigatewayv2/apis.go
@@ -9,6 +9,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/apigatewayv2/types"
 	"github.com/src-bin/substrate/awscfg"
 	"github.com/src-bin/substrate/awscloudwatch"
+	"github.com/src-bin/substrate/jsonutil"
 	"github.com/src-bin/substrate/tagging"
 	"github.com/src-bin/substrate/ui"
 	"github.com/src-bin/substrate/version"
@@ -112,7 +113,40 @@ func EnsureAPI(
 				cfg.MustAccountId(ctx),
 				name,
 			)),
-			Format: aws.String(`$context.identity.sourceIp - - [$context.requestTime] "$context.httpMethod $context.routeKey $context.protocol" $context.status $context.responseLength $context.requestId`), // Apache common log format
+			//Format: aws.String(`$context.identity.sourceIp - - [$context.requestTime] "$context.httpMethod $context.routeKey $context.protocol" $context.status $context.responseLength $context.requestId`), // Apache common log format
+			Format: aws.String(jsonutil.MustOneLineString(map[string]string{
+				"accountId":                     "$context.accountId",
+				"apiId":                         "$context.apiId",
+				"authorizer.error":              "$context.authorizer.error",
+				"authorizer.principalId":        "$context.authorizer.principalId",
+				"awsEndpointRequestId":          "$context.awsEndpointRequestId",
+				"awsEndpointRequestId2":         "$context.awsEndpointRequestId2",
+				"customDomain.basePathMatched":  "$context.customDomain.basePathMatched",
+				"dataProcessed":                 "$context.dataProcessed",
+				"domainName":                    "$context.domainName",
+				"domainPrefix":                  "$context.domainPrefix",
+				"error.message":                 "$context.error.message",
+				"error.responseType":            "$context.error.responseType",
+				"httpMethod":                    "$context.httpMethod",
+				"integration.error":             "$context.integration.error",
+				"integrationErrorMessage":       "$context.integrationErrorMessage",
+				"integration.integrationStatus": "$context.integration.integrationStatus",
+				"integration.latency":           "$context.integration.latency",
+				"integrationLatency":            "$context.integrationLatency",
+				"integration.requestId":         "$context.integration.requestId",
+				"integration.status":            "$context.integration.status",
+				"integrationStatus":             "$context.integrationStatus",
+				"path":                          "$context.path",
+				"protocol":                      "$context.protocol",
+				"requestId":                     "$context.requestId",
+				"requestTime":                   "$context.requestTime",
+				"requestTimeEpoch":              "$context.requestTimeEpoch",
+				"responseLatency":               "$context.responseLatency",
+				"responseLength":                "$context.responseLength",
+				"routeKey":                      "$context.routeKey",
+				"stage":                         "$context.stage",
+				"status":                        "$context.status",
+			})), // almost everything per <https://docs.aws.amazon.com/apigateway/latest/developerguide/http-api-logging-variables.html>
 		},
 		ApiId:      aws.String(api.Id),
 		AutoDeploy: true,
