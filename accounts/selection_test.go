752589d9ebce6f6dbd8c4e9264ee5333d2bd763a
Richard Crowley <r@rcrowley.org>
2023-12-31 21:33:26 -0800

    Catch account selection tests up to Cobra reality

--- accounts/selection_test.go
+++ accounts/selection_test.go
@@ -2,28 +2,13 @@ package accounts
 
 import (
 	"context"
-	"flag"
 	"sort"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/src-bin/substrate/awscfg/testawscfg"
-	"github.com/src-bin/substrate/cmdutil"
 	"github.com/src-bin/substrate/roles"
 )
-
-func TestSelectionAdmin(t *testing.T) {
-	ctx := context.Background()
-	cfg, restore := testawscfg.Test1(roles.Administrator)
-	defer restore()
-	selection := Selection{Admin: true}
-	selected, _, err := selection.Partition(ctx, cfg)
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert(t, len(selected), 1)
-	assert(t, aws.ToString(selected[0].Account.Id), "716893237583")
-}
 
 func TestSelectionAllService(t *testing.T) {
 	ctx := context.Background()
@@ -43,66 +28,27 @@ func TestSelectionAllService(t *testing.T) {
 		accountIds = append(accountIds, aws.ToString(as.Account.Id))
 	}
 	sort.Strings(accountIds)
-	assertSlice(t, accountIds, []string{"306228446141", "765683503745", "832411437665"})
+	assertSlice(t, accountIds, []string{"306228446141", "509660714689", "765683503745", "832411437665"})
 }
 
-func TestSelectionFlagsToSelection1(t *testing.T) {
-	defer cmdutil.RestoreArgs()
-	cmdutil.OverrideArgs(
-		"-domain", "bar", // these must be in alphabetical order because
-		"-domain", "foo", // they will be when they're regurgitated
-		"-environment", "staging",
-		"-management",
-		"-special", "deploy", // same for
-		"-special", "network", // these
-	)
-	f := NewSelectionFlags(SelectionFlagsUsage{"-", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"})
-	flag.Parse()
-	s, err := f.Selection()
+func TestSelectionFoo(t *testing.T) {
+	ctx := context.Background()
+	cfg, restore := testawscfg.Test1(roles.Administrator)
+	defer restore()
+	selection := Selection{
+		Domains:         []string{"foo"},
+		AllEnvironments: true,
+		AllQualities:    true,
+	}
+	selected, _, err := selection.Partition(ctx, cfg)
 	if err != nil {
 		t.Fatal(err)
 	}
-	assert(t, s.AllDomains, false)
-	assertSlice(t, s.Domains, []string{"bar", "foo"})
-	assert(t, s.AllEnvironments, false)
-	assertSlice(t, s.Environments, []string{"staging"})
-	assert(t, s.AllQualities, true) // there's only one quality in test1
-	assertSlice(t, s.Qualities, []string{})
-	assert(t, s.Admin, false)
-	assert(t, s.Management, true)
-	assertSlice(t, s.Specials, []string{"deploy", "network"})
-	assertSlice(t, s.Numbers, []string{})
-}
-
-func TestSelectionFlagsToSelection2(t *testing.T) {
-	f := &SelectionFlags{
-		AllDomains:      aws.Bool(true),
-		Domains:         &cmdutil.StringSliceFlag{},
-		AllEnvironments: aws.Bool(true),
-		Environments:    &cmdutil.StringSliceFlag{},
-		AllQualities:    aws.Bool(false),
-		Qualities:       &cmdutil.StringSliceFlag{"default"},
-		Admin:           aws.Bool(true),
-		Substrate:       aws.Bool(true),
-		Management:      aws.Bool(false),
-		Specials:        &cmdutil.StringSliceFlag{},
-		Numbers:         &cmdutil.StringSliceFlag{"123456789012"},
+	var accountIds []string
+	for _, as := range selected {
+		accountIds = append(accountIds, aws.ToString(as.Account.Id))
 	}
-	s, err := f.Selection()
-	if err != nil {
-		t.Fatal(err)
-	}
-	assert(t, s.AllDomains, *f.AllDomains)
-	assertSlice(t, s.Domains, f.Domains.Slice())
-	assert(t, s.AllEnvironments, *f.AllEnvironments)
-	assertSlice(t, s.Environments, f.Environments.Slice())
-	assert(t, s.AllQualities, *f.AllQualities)
-	assertSlice(t, s.Qualities, f.Qualities.Slice())
-	assert(t, s.Admin, *f.Admin)
-	assert(t, s.Substrate, *f.Substrate)
-	assert(t, s.Management, *f.Management)
-	assertSlice(t, s.Specials, f.Specials.Slice())
-	assertSlice(t, s.Numbers, f.Numbers.Slice())
+	assertSlice(t, accountIds, []string{"765683503745"})
 }
 
 func TestSelectionHumans(t *testing.T) {
