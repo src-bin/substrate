aaad49d4b24b27c218d25dd58dc50fe750646799
Richard Crowley <r@rcrowley.org>
2023-12-31 23:44:04 -0800

    Redirectable output version of cmdutil.Main

--- cmdutil/main.go
+++ cmdutil/main.go
@@ -10,8 +10,15 @@ import (
 )
 
 // Main returns the arguments necessary for a typical subcommand's Main
-// function so that it can be called as Main(awscfg.Main(cmd.Context())).
+// function so that it can be called as Main(cmdutil.Main(cmd.Context())).
 func Main(cmd *cobra.Command, args []string) (context.Context, *awscfg.Config, *cobra.Command, []string, io.Writer) {
+	return MainRedirect(cmd, args, os.Stdout)
+}
+
+// MainRedirect returns the arguments necessary for a typical subcommand's
+// Main function with its output io.Writer redirected to w. Call the Main
+// function as Main(cmdutil.MainRedirect(cmd.Context(), w)).
+func MainRedirect(cmd *cobra.Command, args []string, w io.Writer) (context.Context, *awscfg.Config, *cobra.Command, []string, io.Writer) {
 	ctx := cmd.Context()
-	return ctx, awscfg.Must(awscfg.NewConfig(ctx)), cmd, args, os.Stdout
+	return ctx, awscfg.Must(awscfg.NewConfig(ctx)), cmd, args, w
 }
