093cc6d8b3d779833c223b842def8f0b1ee4451d
Richard Crowley <r@rcrowley.org>
2024-01-23 21:04:11 -0800

    HTML for the /download page in the build

---
+++ tools/download-html.sh
@@ -0,0 +1,24 @@
+set -e -x
+
+VERSION="$(cat "substrate.version")"
+
+cat >"substrate.download.html" <<EOF
+<table border="0" cellpadding="0" cellspacing="16">
+    <tr>
+        <th>macOS (Intel)</th>
+        <td><a href="https://src-bin.com/substrate-$VERSION-darwin-amd64.tar.gz">substrate-$VERSION-darwin-amd64.tar.gz</a></td>
+    </tr>
+    <tr>
+        <th>macOS (Apple Silicon)</th>
+        <td><a href="https://src-bin.com/substrate-$VERSION-darwin-arm64.tar.gz">substrate-$VERSION-darwin-arm64.tar.gz</a></td>
+    </tr>
+    <tr>
+        <th>Linux (Intel/AMD)</th>
+        <td><a href="https://src-bin.com/substrate-$VERSION-linux-amd64.tar.gz">substrate-$VERSION-linux-amd64.tar.gz</a></td>
+    </tr>
+    <tr>
+        <th>Linux (ARM)</th>
+        <td><a href="https://src-bin.com/substrate-$VERSION-linux-arm64.tar.gz">substrate-$VERSION-linux-arm64.tar.gz</a></td>
+    </tr>
+</table>
+EOF
